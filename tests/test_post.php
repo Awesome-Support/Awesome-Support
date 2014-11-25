af6e8b0bf04013ebc197cdf139a3649aef5fb8eb
Julien Liabeuf <julien@liabeuf.fr>
2014-11-25 21:50:46 +0700

    Add unit tests for post functions

--- tests/test_post.php
+++ tests/test_post.php
@@ -8,21 +8,73 @@ class WPAS_Tickets_Tests extends WP_UnitTestCase {
         parent::setUp();
        
         $this->ticket_id = null;
+        $this->reply_id = null;
+
+        $this->ticket_data = array(
+            'post_title'   => 'Test Ticket',
+            'post_name'    => 'Test Ticket',
+            'post_author'  => 1,
+            'post_content' => 'In hac habitasse platea dictumst. Nulla neque dolor, sagittis eget, iaculis quis, molestie non, velit. Nullam cursus lacinia erat. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Donec vitae orci sed dolor rutrum auctor.'
+        );
+
+        $this->reply_data = array(
+            'post_content' => 'Vivamus aliquet elit ac nisl. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium. Nullam vel sem. Praesent nonummy mi in odio.'
+        );
      
     }
 
+    function test_wpas_open_ticket() {
+        $data = array(
+            'wpas_nonce' => wp_create_nonce( 'new_ticket' ),
+            'wpas_title'   => 'Test Ticket',
+            'wpas_message' => 'In hac habitasse platea dictumst. Nulla neque dolor, sagittis eget, iaculis quis, molestie non, velit. Nullam cursus lacinia erat. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Donec vitae orci sed dolor rutrum auctor.'
+        );
+        $ticket_id = wpas_open_ticket( $data );
+        $this->assertInternalType( 'int', $ticket_id );
+    }
+
     function test_wpas_insert_ticket() {
-
-    	$data = array(
-			'post_title'   => 'Test Ticket',
-			'post_name'    => 'Test Ticket',
-			'post_author'  => 1,
-			'post_content' => 'In hac habitasse platea dictumst. Nulla neque dolor, sagittis eget, iaculis quis, molestie non, velit. Nullam cursus lacinia erat. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Donec vitae orci sed dolor rutrum auctor.',
-    	);
-
-		$this->ticket_id = wpas_insert_ticket( $data, false );
-
-    	$this->assertInternalType( "int", $this->ticket_id );
+		$ticket_id = wpas_insert_ticket( $this->ticket_data, false );
+    	$this->assertInternalType( 'int', $ticket_id );
 	}
+
+    function test_wpas_add_reply() {
+        $ticket_id = wpas_insert_ticket( $this->ticket_data, false );
+        $reply_id  = wpas_add_reply( $this->reply_data, $ticket_id );
+        $this->assertInternalType( 'int', $reply_id );
+    }
+
+    function test_wpas_insert_reply() {
+        $ticket_id = wpas_insert_ticket( $this->ticket_data, false );
+        $reply_id  = wpas_insert_reply( $this->reply_data, $ticket_id );
+        $this->assertInternalType( 'int', $reply_id );
+    }
+
+    function test_wpas_edit_reply() {
+        $ticket_id = wpas_insert_ticket( $this->ticket_data, false );
+        $reply_id  = wpas_insert_reply( $this->reply_data, $ticket_id );
+        $edited    = wpas_edit_reply( $reply_id, 'Vivamus aliquet elit ac nisl.' );
+        $this->assertInternalType( 'int', $edited );
+    }
+
+    function test_wpas_mark_reply_read() {
+        $ticket_id = wpas_insert_ticket( $this->ticket_data, false );
+        $reply_id  = wpas_insert_reply( $this->reply_data, $ticket_id );
+        $edited    = wpas_mark_reply_read( $reply_id );
+        $this->assertInternalType( 'int', $edited );
+    }
+
+    function test_wpas_get_replies() {
+        $ticket_id  = wpas_insert_ticket( $this->ticket_data, false );
+        $reply_id   = wpas_insert_reply( $this->reply_data, $ticket_id );
+        $reply_id_2 = wpas_insert_reply( $this->reply_data, $ticket_id );
+        $replies    = wpas_get_replies( $ticket_id );
+        $this->assertNotEmpty( $replies );
+    }
+
+    function test_wpas_find_agent() {
+        $agent = wpas_find_agent();
+        $this->assertInternalType( 'int', $agent );
+    }
  
 }
