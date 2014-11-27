cad092044b7dfe37fe549101f7ac6432ae39ac68
Julien Vernet <julien@vernet.me>
2014-11-27 17:17:16 +0700

    Check for TinyMCE & add notify user if TinyMCE is empty (See issue #6)

--- assets/public/js/public.js
+++ assets/public/js/public.js
@@ -3,10 +3,38 @@
 
 	$(function () {
 
-		$('.wpas-form').submit(function (event) {
-			var submitBtn = $('[type="submit"]', $(this));
-			submitBtn.prop('disabled', true).text(submitBtn.data('onsubmit'));
-		});
+		/* http://codeblow.com/questions/method-to-check-whether-tinymce-is-active-in-wordpress/ */
+		if (typeof tinyMCE != "undefined") {
+
+			$('.wpas-form').submit(function (event) {
+				var submitBtn = $('[type="submit"]', $(this));
+				var editorContent = tinyMCE.activeEditor.getContent();
+				if (editorContent === '' || editorContent === null) {
+
+					/* Highlight the active editor */
+					$(tinyMCE.activeEditor.getBody()).css('background-color', '#ffeeee');
+
+					/* Alert the user */
+					alert('You can\'t submit an empty ticket reply.');
+					$(tinyMCE.activeEditor.getBody()).css('background-color', '');
+
+					/* Focus on editor */
+					tinyMCE.activeEditor.focus();
+
+					return false;
+				} else {
+					submitBtn.prop('disabled', true).text(submitBtn.data('onsubmit'));
+				}
+			});
+
+		} else {
+
+			$('.wpas-form').submit(function (event) {
+				var submitBtn = $('[type="submit"]', $(this));
+				submitBtn.prop('disabled', true).text(submitBtn.data('onsubmit'));
+			});
+
+		}
 
 	});
 
