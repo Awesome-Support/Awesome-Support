ca5c50d789c39fe656ff1defddda3adbc18c9ddd
Julien Liabeuf <julien@liabeuf.fr>
2014-12-10 16:05:50 +0700

    Uninstall process is now working

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 /**
  * Fired when the plugin is uninstalled.
  *
- * @package   Awesome_Support
+ * @package   Awesome Support/Uninstallation
  * @author    Julien Liabeuf <julien@liabeuf.Fr>
  * @license   GPL-2.0+
  * @link      http://themeavenue.net
@@ -26,8 +26,7 @@ if ( is_multisite() ) {
 		}
 	}
 }
-else
-{
+else {
 	wpas_uninstall();
 }
 
@@ -42,89 +41,87 @@ else
  */
 function wpas_uninstall() {
 
-	$options = get_option( 'wpas_options' );
+	$options = maybe_unserialize( get_option( 'wpas_options' ) );
 
-	if ( !isset( $options['delete_data'] ) ) {
-		return;
-	}
+	/* Make sure that the user wants to remove all the data. */
+	if ( isset( $options['delete_data'] ) || '1' === $options['delete_data'] ) {
 
-	if ( '1' !== $options['delete_data'] ) {
-		return;
-	}
+		/* Remove all plugin options. */
+		delete_option( 'wpas_options' );
+		delete_option( 'wpas_db_version' );
+		delete_option( 'wpas_version' );
 
-	delete_option( 'wpas_options' );
-	delete_option( 'wpas_db_version' );
-	delete_option( 'wpas_version' );
+		/* Delete the plugin pages.	 */
+		wp_delete_post( intval( $options['ticket_submit'] ), true );
+		wp_delete_post( intval( $options['ticket_list'] ), true );
 
-	/**
-	 * Delete the plugin pages.
-	 */
-	wp_delete_post( intval( $options['ticket_submit'] ), true );
-	wp_delete_post( intval( $options['ticket_list'] ), true );
+		/**
+		 * Delete all posts from all custom post types
+		 * that the plugin created.
+		 */
+		$args = array(
+			'post_type'              => array( 'ticket', 'ticket_reply', 'ticket_history', 'ticket_log' ),
+			'post_status'            => array( 'any', 'trash', 'auto-draft' ),
+			'posts_per_page'         => -1,
+			'no_found_rows'          => true,
+			'cache_results'          => false,
+			'update_post_term_cache' => false,
+			'update_post_meta_cache' => false,
+			
+		);
 
-	/**
-	 * Delete all posts from all custom post types
-	 * that the plugin created.
-	 */
-	$args = array(
-		'post_type'              => array( 'ticket', 'ticket_reply', 'ticket_history', 'ticket_log' ),
-		'post_status'            => 'any',
-		'posts_per_page'         => -1,
-		'no_found_rows'          => true,
-		'cache_results'          => false,
-		'update_post_term_cache' => false,
-		'update_post_meta_cache' => false,
-		
-	);
+		$posts = new WP_Query( $args );
 
-	$posts = new WP_Query( $args );
-
-	foreach ( $posts->posts as $post ) {
-		if ( 'attachment' === $post->post_type ) {
-			wp_delete_attachment( $post->ID, true );
-		} else {
-			wp_delete_post( $post->ID, true );
-		}
-	}
-
-	/**
-	 * Delete all products if the taxonomy
-	 * was in use on this install.
-	 */
-	if ( '1' === $options['support_products'] ) {
-
-		$products = get_terms( WPAS_PRODUCT_SLUG );
-
-		foreach ( $products as $product ) {
-			wp_delete_term( $product->term_id, WPAS_PRODUCT_SLUG );
-		}
-
-	}
-
-	/**
-	 * Delete all terms from custom taxonomies
-	 * that might have been added.
-	 */
-	global $wpas_cf;
-
-	$fields = $wpas_cf->get_custom_fields();
-
-	foreach ( $fields as $field ) {
-
-		if ( 'taxonomy' === $field['args']['callback'] ) {
-
-			$custom_taxos = get_terms( $field['name'] );
-
-			foreach ( $custom_taxos as $taxo ) {
-				wp_delete_term( $taxo->term_id, $field['name'] );
+		foreach ( $posts->posts as $post ) {
+			if ( 'attachment' === $post->post_type ) {
+				wp_delete_attachment( $post->ID, true );
+			} else {
+				wp_delete_post( $post->ID, true );
 			}
+		}
 
+		/* Delete all tag terms. */
+		wpas_delete_taxonomy( 'ticket-tag' );
+
+		/**
+		 * Delete all products if the taxonomy
+		 * was in use on this install.
+		 */
+		if ( '1' === $options['support_products'] ) {
+			wpas_delete_taxonomy( 'product' );
 		}
 
 	}
 
-	/**
-	 * Delete all tags terms.
-	 */
+}
+
+/**
+ * Delete all terms of the given taxonomy.
+ *
+ * As the get_terms function is not availabel during uninstall
+ * (because the taxonomies are not registered), we need to work
+ * directly with the $wpdb class. The function gets all taxonomy terms
+ * and deletes them one by one.
+ *
+ * @since  3.0.0
+ * @param  string $taxonomy Name of the taxonomy to delete
+ * @link   http://wordpress.stackexchange.com/a/119353
+ * @return void
+ */
+function wpas_delete_taxonomy( $taxonomy ) {
+
+	global $wpdb;
+
+    $query = 'SELECT t.name, t.term_id
+            FROM ' . $wpdb->terms . ' AS t
+            INNER JOIN ' . $wpdb->term_taxonomy . ' AS tt
+            ON t.term_id = tt.term_id
+            WHERE tt.taxonomy = "' . $taxonomy . '"';
+
+    $terms = $wpdb->get_results($query);
+
+    foreach ( $terms as $term ) {
+        wp_delete_term( $term->term_id, $taxonomy );
+    }
 
 }
