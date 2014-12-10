1725547c956b9cf303eb9ead9b42b646211ad750
Julien Liabeuf <julien@liabeuf.fr>
2014-12-10 14:44:07 +0700

    Hardcode the plugin slug

--- includes/functions-custom-fields.php
+++ includes/functions-custom-fields.php
@@ -22,7 +22,7 @@
  */
 function wpas_get_field_value( $field_name ) {
 
-	$meta = get_post_meta( get_the_ID(), '_' . WPAS_SLUG . "_$field_name", true );
+	$meta = get_post_meta( get_the_ID(), '_wpas_' . $field_name, true );
 
 	if ( isset( $_SESSION['wpas_submission_form'] ) && is_array( $_SESSION['wpas_submission_form'] ) && array_key_exists( $field_name, $_SESSION['wpas_submission_form'] ) ) {
 		$value = $_SESSION['wpas_submission_form'][$field_name];
