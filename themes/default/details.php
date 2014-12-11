014a8002699889b25e0408457f16d4433d1207a0
Julien Liabeuf <julien@liabeuf.fr>
2014-12-11 10:52:03 +0700

    Add the date and time on all replies

--- themes/default/details.php
+++ themes/default/details.php
@@ -44,10 +44,9 @@ $author = get_user_by( 'id', $post->post_author );
 						</div>
 						<div class="wpas-reply-time">
 							<time class="wpas-timestamp" datetime="<?php echo get_the_date( 'Y-m-d\TH:i:s' ) . wpas_get_offset_html5(); ?>">
-								<span class="wpas-human-date">
-									<?php echo date( get_option( 'date_format' ), strtotime( $post->post_date ) ) . ' ' . date( get_option( 'time_format' ) ); ?>
-								</span>
-								<?php printf( __( '%s ago', 'wpas' ), human_time_diff( get_the_time( 'U', $post->ID ), current_time( 'timestamp' ) ) ); ?></time>
+								<span class="wpas-human-date"><?php echo date( get_option( 'date_format' ), strtotime( $post->post_date ) ) . ' ' . date( get_option( 'time_format' ) ); ?></span>
+								<span class="wpas-date-ago"><?php printf( __( '%s ago', 'wpas' ), human_time_diff( get_the_time( 'U', $post->ID ), current_time( 'timestamp' ) ) ); ?></span>
+							</time>
 						</div>
 					</div>
 
@@ -114,7 +113,10 @@ $author = get_user_by( 'id', $post->post_author );
 									<strong class="wpas-profilename"><?php echo get_the_author_meta( 'user_nicename' ); ?></strong>
 								</div>
 								<div class="wpas-reply-time">
-									<time class="wpas-timestamp" datetime="<?php echo get_the_date( 'Y-m-d\TH:i:s' ) . wpas_get_offset_html5(); ?>"><span class="wpas-human-date"><?php echo date( get_option( 'date_format' ), strtotime( get_the_date() ) ); ?></span><span class="wpas-date-ago"><?php printf( __( '%s ago', 'wpas' ), $time_ago ); ?></span></time>
+									<time class="wpas-timestamp" datetime="<?php echo get_the_date( 'Y-m-d\TH:i:s' ) . wpas_get_offset_html5(); ?>">
+										<span class="wpas-human-date"><?php echo date( get_option( 'date_format' ), strtotime( get_the_date() ) ) . ' ' . date( get_option( 'time_format' ) ); ?></span>
+										<span class="wpas-date-ago"><?php printf( __( '%s ago', 'wpas' ), $time_ago ); ?></span>
+									</time>
 								</div>
 							</div>
 
