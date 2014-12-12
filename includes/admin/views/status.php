9e6ba2efb283389d010f12fc1fe976b84b2a1d89
Julien Liabeuf <julien@liabeuf.fr>
2014-12-12 12:08:50 +0700

    Add a tools tab in the system page

--- includes/admin/views/status.php
+++ includes/admin/views/status.php
@@ -2,6 +2,7 @@
 
 	<h2 class="nav-tab-wrapper">
 		<a href="<?php echo add_query_arg( array( 'post_type' => 'ticket', 'page' => 'wpas-status', 'tab' => 'status' ), admin_url( 'edit.php' ) ); ?>" class="nav-tab <?php if ( !isset( $_GET['tab'] ) || 'status' === $_GET['tab'] ): ?> nav-tab-active<?php endif; ?>"><?php _e( 'System Status', 'wpas' ); ?></a>
+		<a href="<?php echo add_query_arg( array( 'post_type' => 'ticket', 'page' => 'wpas-status', 'tab' => 'tools' ), admin_url( 'edit.php' ) ); ?>" class="nav-tab <?php if ( isset( $_GET['tab'] ) && 'tools' === $_GET['tab'] ): ?> nav-tab-active<?php endif; ?>"><?php _e( 'Tools', 'wpas' ); ?></a>
 	</h2>
 
 	<?php
@@ -10,7 +11,7 @@
 	} else {
 		switch( $_GET['tab'] ) {
 			case 'tools':
-
+				require_once( WPAS_PATH . 'includes/admin/views/system-tools.php' );
 			break;
 
 			default:
---
+++ includes/admin/views/system-tools.php
@@ -0,0 +1,24 @@
+<table class="widefat wpas-system-tools-table" id="wpas-system-tools">
+	<thead>
+		<tr>
+			<th data-override="key" class="row-title"><?php _e( 'Tools', 'wpas' ); ?></th>
+			<th data-override="value"></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="row-title"><label for="tablecell"><?php _e( 'Activity Meta', 'wpas' ); ?></label></td>
+			<td>
+				<a href="" class="button-secondary"><?php _e( 'Clear', 'wpas' ); ?></a> 
+				<div class="wpas-system-tools-desc"><?php _e( 'Clear all transiants containing tickets activity metas.', 'wpas' ); ?></div>
+			</td>
+		</tr>
+		<tr>
+			<td class="row-title"><label for="tablecell"><?php _e( 'Custom Taxonomies', 'wpas' ); ?></label></td>
+			<td>
+				<a href="" class="button-secondary"><?php _e( 'Clear', 'wpas' ); ?></a> 
+				<div class="wpas-system-tools-desc"><?php _e( 'Clear all terms from all custom taxonomies.', 'wpas' ); ?></div>
+			</td>
+		</tr>
+	</tbody>
+</table>
