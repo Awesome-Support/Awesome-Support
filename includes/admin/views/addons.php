3233ac917215ddbd5e13d3286384303845fedac5
Julien Liabeuf <julien@liabeuf.fr>
2014-12-17 14:45:18 +0700

    Clean the mess after failed merge. See commits 6902750bf95606aa9613b9dd00aec91d192e140b, b7bb124379dd249f4253f0dbfa764a4f75d94df3 and 460a47f5ccf8193a4a85a042576d6f83c47d766f

--- includes/admin/views/addons.php
+++ includes/admin/views/addons.php
@@ -30,6 +30,23 @@ if ( false === $items ) {
 ?>
 
 <style type="text/css">
+.wpas-addon-all .wpas-addon-item {
+	margin-bottom: 40px;
+	padding-bottom: 40px;
+	border-bottom: 1px solid #ddd;
+}
+.wpas-addon-all .wpas-addon-item:last-child {
+	margin-bottom: 0px;
+	border-bottom: none;
+}
+.wpas-addon-all .last-feature {
+	padding-top: 40px;
+}
+.wpas-addon-img-wrap {
+	width: 100%;
+	min-height: 311px;
+	background: #ddd url('<?php echo admin_url(); ?>/images/wpspin_light-2x.gif') no-repeat 50% 50%;
+}
 .wpas-addon-img {
 	box-shadow: 0px 0px 25px rgba(0,0,0,0.25);
 }
@@ -45,39 +62,40 @@ if ( false === $items ) {
 	<h1>Addons</h1>
 	<div class="about-text">Even though Awesome Support has a lot of built-in features, it is impossible to make everyone happy. This is why we have lots of addons to help you tailor your support system.</div>
 
-	<?php
-	if ( false === $items ):
-		?><p>To check out all our addons please visit <a href="http://getawesomesupport.com/addons" target="_blank">http://getawesomesupport.com/addons</a></p><?php
-	else:
-		// wpas_debug_display( $items );
-		foreach ( $items as $key => $item ):
+	<div class="changelog wpas-addon-all">
+		<?php
+		if ( false === $items ):
+			?><p>To check out all our addons please visit <a href="http://getawesomesupport.com/addons" target="_blank">http://getawesomesupport.com/addons</a></p><?php
+		else:
+			// wpas_debug_display( $items );
+			foreach ( $items as $key => $item ):
 
-			/* Get the item price */
-			$price = false;
+				/* Get the item price */
+				$price = false;
 
-			/* This item has a fixed price */
-			if ( isset( $item->pricing->amount ) ) {
-				$price = number_format( $item->pricing->amount, 0 );
-			}
-
-			/* This item has variable pricing */
-			else {
-				if ( isset( $item->pricing->singlesite ) ) {
-					$price = number_format( $item->pricing->singlesite, 0 );
+				/* This item has a fixed price */
+				if ( isset( $item->pricing->amount ) ) {
+					$price = number_format( $item->pricing->amount, 0 );
 				}
-			} ?>
-			<div class="feature-section col two-col wpas-addon-item" id="wpas-addon-item-<?php echo intval( $item->info->id ); ?>">
-				<div class="col-1">
-					<?php if ( !empty( $item->info->thumbnail ) ): ?><img class="wpas-addon-img" src="<?php echo esc_url( $item->info->thumbnail ); ?>"><?php endif; ?>
-				</div>
-				<div class="col-2 last-feature">
-					<h3><?php echo esc_attr( $item->info->title ); ?> <small class="wpas-addon-item-pricing">from <?php if ( false !== $price ): ?><strong>$<?php echo $price; ?></strong><?php endif; ?></small></h3>
-					<p><?php if ( !empty( $item->info->excerpt ) ): echo wpautop( $item->info->excerpt ); endif; ?></p>
-					<a class="button-primary" href="<?php echo esc_url( $item->info->link ); ?>" target="_blank">View details</a>
-				</div>
-			</div>
-			<hr>
-		<?php endforeach;
 
-	endif; ?>
+				/* This item has variable pricing */
+				else {
+					if ( isset( $item->pricing->singlesite ) ) {
+						$price = number_format( $item->pricing->singlesite, 0 );
+					}
+				} ?>
+				<div class="feature-section col two-col wpas-addon-item" id="wpas-addon-item-<?php echo intval( $item->info->id ); ?>">
+					<div class="col-1">
+						<div class="wpas-addon-img-wrap"><?php if ( !empty( $item->info->thumbnail ) ): ?><img class="wpas-addon-img" src="<?php echo esc_url( $item->info->thumbnail ); ?>"><?php endif; ?></div>
+					</div>
+					<div class="col-2 last-feature">
+						<h3><?php echo esc_attr( $item->info->title ); ?> <small class="wpas-addon-item-pricing">from <?php if ( false !== $price ): ?><strong>$<?php echo $price; ?></strong><?php endif; ?></small></h3>
+						<p><?php if ( !empty( $item->info->excerpt ) ): echo wpautop( $item->info->excerpt ); endif; ?></p>
+						<a class="button-primary" href="<?php echo esc_url( $item->info->link ); ?>" target="_blank">View details</a>
+					</div>
+				</div>
+			<?php endforeach;
+
+		endif; ?>
+	</div>
 </div>
