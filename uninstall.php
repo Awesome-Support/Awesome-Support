23812dc0f9718d6deb999bfe5987eba811f6ecee
Ninos Ego <me@ninosego.de>
2014-12-24 05:18:39 +0100

    CS

--- uninstall.php
+++ uninstall.php
@@ -125,17 +125,17 @@ function wpas_delete_taxonomy( $taxonomy ) {
 
 	global $wpdb;
 
-    $query = 'SELECT t.name, t.term_id
-            FROM ' . $wpdb->terms . ' AS t
-            INNER JOIN ' . $wpdb->term_taxonomy . ' AS tt
-            ON t.term_id = tt.term_id
-            WHERE tt.taxonomy = "' . $taxonomy . '"';
+	$query = 'SELECT t.name, t.term_id
+			FROM ' . $wpdb->terms . ' AS t
+			INNER JOIN ' . $wpdb->term_taxonomy . ' AS tt
+			ON t.term_id = tt.term_id
+			WHERE tt.taxonomy = "' . $taxonomy . '"';
 
-    $terms = $wpdb->get_results($query);
+	$terms = $wpdb->get_results($query);
 
-    foreach ( $terms as $term ) {
-        wp_delete_term( $term->term_id, $taxonomy );
-    }
+	foreach ( $terms as $term ) {
+		wp_delete_term( $term->term_id, $taxonomy );
+	}
 
 }
 
