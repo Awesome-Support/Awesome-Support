1c45b721aa5dbeb389d393b1fe4989a3a78d94f6
Julien Liabeuf <julien@liabeuf.fr>
2015-01-21 10:47:55 +0700

    Add support for WP eCommerce

--- includes/integrations/loader.php
+++ includes/integrations/loader.php
@@ -14,3 +14,7 @@ add_action( 'init', array( 'WPAS_Product_EDD', 'get_instance' ), 11, 0 );
 /* iThemes Exchange */
 require_once( WPAS_PATH . 'includes/integrations/class-product-exchange.php' );
 add_action( 'init', array( 'WPAS_Product_Exchange', 'get_instance' ), 11, 0 );
+
+/* WP eCommerce */
+require_once( WPAS_PATH . 'includes/integrations/class-product-wp-ecommerce.php' );
+add_action( 'init', array( 'WPAS_Product_WP_Ecommerce', 'get_instance' ), 11, 0 );
