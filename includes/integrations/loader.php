59225dd8ba4c1c7b767b076e599bcdaa734962e5
Julien Liabeuf <julien@liabeuf.fr>
2015-01-21 10:38:29 +0700

    Damn copy/paste

--- includes/integrations/loader.php
+++ includes/integrations/loader.php
@@ -11,6 +11,6 @@ add_action( 'init', array( 'WPAS_Product_WooCommerce', 'get_instance' ), 11, 0 )
 require_once( WPAS_PATH . 'includes/integrations/class-product-edd.php' );
 add_action( 'init', array( 'WPAS_Product_EDD', 'get_instance' ), 11, 0 );
 
-/* Easy Digital Downloads */
+/* iThemes Exchange */
 require_once( WPAS_PATH . 'includes/integrations/class-product-exchange.php' );
 add_action( 'init', array( 'WPAS_Product_Exchange', 'get_instance' ), 11, 0 );
