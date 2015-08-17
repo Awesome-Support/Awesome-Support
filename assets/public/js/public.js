df7c5f33e24c39f6fc67452bf0cfd412831a0afb
SiamKreative <julien@vernet.me>
2015-08-17 17:08:46 +0700

    Limit maximum items on a multiple input + Check the file size and warn users (See #167)

--- assets/public/js/public.js
+++ assets/public/js/public.js
@@ -91,20 +91,39 @@
 		}
 
 		/*
-		Limit maximum items on a multiple input
-		http://stackoverflow.com/a/10105631
+		Clear the file input with jQuery
 		http://stackoverflow.com/a/13351234
+		 */
+		function clearFileInput(input) {
+			input.wrap('<form>').parent('form').trigger('reset');
+			input.unwrap();
+		}
+
+		/*
+		Limit maximum items on a multiple input
+		http://stackoverflow.com/a/10105631		
 		 */
 		if (typeof wpas !== 'undefined' && wpas.fileUploadMax) {
 			var $fileUpload = $('#wpas-file-upload');
 			$fileUpload.on('change', function (event) {
 				event.preventDefault();
+
+				// Check file input size with jQuery | http://stackoverflow.com/a/3937404
+				var bigFiles = [];
+				$.each($fileUpload.get(0).files, function (index, val) {
+					if (val.size > wpas.fileUploadSize) {
+						bigFiles.push(val.name);
+					}
+				});
+				if (bigFiles.length !== 0) {
+					alert(wpas.fileUploadMaxSizeError[0] + '\n\n' + bigFiles.join('\n') + '.\n\n' + wpas.fileUploadMaxSizeError[1]);
+					clearFileInput($fileUpload);
+				}
+
+				// Check if not uploading too many files
 				if (parseInt($fileUpload.get(0).files.length) > parseInt(wpas.fileUploadMax, 10)) {
-					// Warn the user
 					alert(wpas.fileUploadMaxError);
-					// Clear the file input
-					$fileUpload.wrap('<form>').parent('form').trigger('reset');
-					$fileUpload.unwrap();
+					clearFileInput($fileUpload);
 				}
 			});
 		}
