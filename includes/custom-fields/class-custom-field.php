65f4b5344ef39fe6f871100b921eb918b2dd66a6
Julien Liabeuf <julien@liabeuf.fr>
2015-08-25 15:26:01 +0700

    Filter field attributes

--- includes/custom-fields/class-custom-field.php
+++ includes/custom-fields/class-custom-field.php
@@ -448,7 +448,7 @@ class WPAS_Custom_Field {
 			array_push( $atts, 'required' );
 		}
 
-		$field = str_replace( '{{atts}}', implode( ' ', $atts ), $field );
+		$field = str_replace( '{{atts}}', implode( ' ', apply_filters( 'wpas_cf_field_atts', $atts, $field ) ), $field );
 		$field = str_replace( '{{label_atts}}', implode( ' ', $label_atts ), $field );
 		$field = str_replace( '{{label}}', $this->get_field_label(), $field );
 
