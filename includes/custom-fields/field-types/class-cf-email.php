590f3e49b4ead6ba47b4dee80a57ce6b141e2adf
Julien Liabeuf <julien@liabeuf.fr>
2015-08-25 13:57:40 +0700

    Add email field type

---
+++ includes/custom-fields/field-types/class-cf-email.php
@@ -0,0 +1,38 @@
+<?php
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class WPAS_CF_Email extends WPAS_Custom_Field {
+
+	/**
+	 * Return the field markup for the front-end.
+	 *
+	 * @return string Field markup
+	 */
+	public function display() {
+		return sprintf( '<label {{label_atts}}>{{label}}</label><input type="email" value="%s" {{atts}}>', $this->populate() );
+	}
+
+	/**
+	 * Return the field markup for the admin.
+	 *
+	 * This method is only used if the current user
+	 * has the capability to edit the field.
+	 */
+	public function display_admin() {
+		return $this->display();
+	}
+
+	/**
+	 * Return the field markup for the admin.
+	 *
+	 * This method is only used if the current user
+	 * doesn't have the capability to edit the field.
+	 */
+	public function display_no_edit() {
+		return sprintf( '<div class="wpas-cf-noedit-wrapper"><div id="%s-label" class="wpas-cf-label">%s</div><div id="%s-value" class="wpas-cf-value">%s</div></div>', $this->get_field_id(), $this->get_field_title(), $this->get_field_id(), $this->get_field_value() );
+	}
+
+}
