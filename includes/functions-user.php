21a890e8445db2766eb8216758d1f996ec1d40ab
Julien Liabeuf <julien@liabeuf.fr>
2015-09-16 17:18:56 +0700

    Use the new error management system for login issues - fixes #201

--- includes/functions-user.php
+++ includes/functions-user.php
@@ -211,7 +211,8 @@ function wpas_try_login() {
 
 		if ( is_wp_error( $login ) ) {
 			$error = $login->get_error_message();
-			wp_redirect( add_query_arg( array( 'message' => urlencode( base64_encode( json_encode( $error ) ) ) ), get_permalink( $post->ID ) ) );
+			wpas_add_error( 'login_failed', $error );
+			wp_redirect( wp_sanitize_redirect( get_permalink( $post->ID ) ) );
 			exit;
 		}
 
@@ -219,13 +220,15 @@ function wpas_try_login() {
 
 		if ( is_wp_error( $login ) ) {
 			$error = $login->get_error_message();
-			wp_redirect( add_query_arg( array( 'message' => urlencode( base64_encode( json_encode( $error ) ) ) ), get_permalink( $post->ID ) ) );
+			wpas_add_error( 'login_failed', $error );
+			wp_redirect( wp_sanitize_redirect( get_permalink( $post->ID ) ) );
 			exit;
 		} elseif ( is_a( $login, 'WP_User' ) ) {
 			wp_redirect( get_permalink( $post->ID ) );
 			exit;
 		} else {
-			wp_redirect( add_query_arg( array( 'message' => urlencode( base64_encode( json_encode( __( 'We were unable to log you in for an unknown reason.', 'wpas' ) ) ) ) ), get_permalink( $post->ID ) ) );
+			wpas_add_error( 'login_failed', __( 'We were unable to log you in for an unknown reason.', 'wpas' ) );
+			wp_redirect( wp_sanitize_redirect( get_permalink( $post->ID ) ) );
 			exit;
 		}
 
