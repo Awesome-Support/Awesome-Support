b789a617a35e80416db7b4730fe87a37ec2cf747
Julien Liabeuf <julien@liabeuf.fr>
2015-10-06 10:29:56 +0700

    Assign the ticket at the very last minute otherwise it's impossible to get the ticket product (reverted from commit ac983a517ef677a582721dd0032f99454fb3d278)

--- includes/functions-post.php
+++ includes/functions-post.php
@@ -245,21 +245,14 @@ function wpas_insert_ticket( $data = array(), $post_id = false, $agent_id = fals
 		$agent_id = wpas_find_agent( $ticket_id );
 	}
 
-	/**
-	 * Fire wpas_after_open_ticket just after the post is successfully submitted.
-	 */
-	do_action( 'wpas_open_ticket_after', $ticket_id, $data );
-
 	/* Assign an agent to the ticket */
 	wpas_assign_ticket( $ticket_id, apply_filters( 'wpas_new_ticket_agent_id', $agent_id, $ticket_id, $agent_id ), false );
 
 	/**
-	 * Fire wpas_open_ticket_after_assigned after the post is successfully submitted and has been assigned to an agent.
-	 *
-	 * @since 3.2.6
+	 * Fire wpas_after_open_ticket just after the post is successfully submitted.
 	 */
-	do_action( 'wpas_open_ticket_after_assigned', $ticket_id, $data );
-
+	do_action( 'wpas_open_ticket_after', $ticket_id, $data );
+	
 	return $ticket_id;
 
 }
