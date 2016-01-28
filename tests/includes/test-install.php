c6358e287a5d34e35b9532fb5c16a07bd70c6039
Julien Liabeuf <julien@liabeuf.fr>
2016-01-28 13:54:39 +0700

    Test users capabilities after install

--- tests/includes/test-install.php
+++ tests/includes/test-install.php
@@ -1,4 +1,5 @@
 <?php
+
 class WPAS_Test_Functions_Install extends WP_UnitTestCase {
 
 	private $plugin;
@@ -32,6 +33,41 @@ class WPAS_Test_Functions_Install extends WP_UnitTestCase {
 
 	function test_db_version() {
 		$this->assertEquals( WPAS_DB_VERSION, get_option( 'wpas_db_version' ) );
+	}
+
+	function test_admin_capability() {
+
+		$admin = new WP_User( 1 );
+		$admin->set_role( 'administrator' );
+		$open = user_can( $admin, 'create_ticket' );
+		$edit = user_can( $admin, 'edit_ticket' );
+
+		$this->assertTrue( $open );
+		$this->assertTrue( $edit );
+	}
+
+	function test_agent_capability() {
+
+		$agent = new WP_User( 1 );
+		$agent->set_role( 'wpas_agent' );
+		$open = user_can( $agent, 'create_ticket' );
+		$edit = user_can( $agent, 'edit_ticket' );
+		$cap  = user_can( $agent, 'edit_ticket' );
+
+
+		$this->assertTrue( $open );
+		$this->assertTrue( $edit );
+	}
+
+	function test_user_capability() {
+
+		$user = new WP_User( 1 );
+		$user->set_role( 'wpas_user' );
+		$open = user_can( $user, 'create_ticket' );
+		$edit = user_can( $user, 'edit_ticket' );
+
+		$this->assertTrue( $open );
+		$this->assertFalse( $edit );
 	}
 
 }
