0ea1ef41754b66a508b4ab371269539eefb20450
Julien Liabeuf <julien@liabeuf.fr>
2016-03-11 10:10:58 +0700

    Always reset database before tests and add progress info

--- tests/bin/install.php
+++ tests/bin/install.php
@@ -15,11 +15,10 @@ require_once $config_file_path;
 require_once $config_dir . '/lib/functions.php';
 
 $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-$_SERVER['HTTP_HOST'] = WP_TESTS_DOMAIN;
-$PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
+$_SERVER['HTTP_HOST']       = WP_TESTS_DOMAIN;
+$PHP_SELF                   = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
 
 require_once ABSPATH . '/wp-settings.php';
-
 require_once ABSPATH . '/wp-admin/includes/upgrade.php';
 require_once ABSPATH . '/wp-includes/wp-db.php';
 
@@ -29,30 +28,51 @@ $wpdb->suppress_errors();
 $wpdb->hide_errors();
 $installed = $wpdb->get_var( "SELECT option_value FROM $wpdb->options WHERE option_name = 'siteurl'" );
 
-if ( $installed && file_exists( WP_TESTS_VERSION_FILE ) ) {
+if ( $installed ) {
+	echo "Deleting previous test data…" . PHP_EOL;
+	$reset = $wpdb->query( 'DROP DATABASE ' . DB_NAME . ";" );
+	if ( $reset ) {
+		echo "[OK] Previous data deleted.\n" . PHP_EOL;
+	} else {
+		echo "[ERROR] Previous data could not be deleted. Tests results might not be accurate.\n" . PHP_EOL;
+	}
+}
+
+echo "Installing fresh database…" . PHP_EOL;
+
+$wpdb->query( 'SET storage_engine = INNODB;' );
+$database = $wpdb->query( 'CREATE DATABASE ' . DB_NAME . ";" );
+$wpdb->select( DB_NAME, $wpdb->dbh );
+
+if ( $database ) {
+	echo "[OK] Fresh database installed.\n" . PHP_EOL;
+} else {
+	echo "[ERROR] Fresh database could not be installed. Tests might fail.\n" . PHP_EOL;
+}
+
+if ( file_exists( WP_TESTS_VERSION_FILE ) ) {
 	$installed_version_hash = file_get_contents( WP_TESTS_VERSION_FILE );
 	if ( $installed_version_hash == test_version_check_hash() ) {
 		return;
 	}
 }
-$wpdb->query( 'SET storage_engine = INNODB;' );
-$wpdb->query( 'DROP DATABASE IF EXISTS '.DB_NAME.";" );
-$wpdb->query( 'CREATE DATABASE '.DB_NAME.";" );
-$wpdb->select( DB_NAME, $wpdb->dbh );
 
 echo "Installing Awesome Support…" . PHP_EOL;
+
 wp_install( WP_TESTS_TITLE, 'admin', WP_TESTS_EMAIL, true, '', 'a' );
 
-if ( defined('WP_ALLOW_MULTISITE') && WP_ALLOW_MULTISITE ) {
-	echo "Installing network…" .PHP_EOL;
+if ( defined( 'WP_ALLOW_MULTISITE' ) && WP_ALLOW_MULTISITE ) {
+	
+	echo "Installing network…" . PHP_EOL;
 
 	define( 'WP_INSTALLING_NETWORK', true );
 	//wp_set_wpdb_vars();
 	// We need to create references to ms global tables to enable Network.
-	foreach ( $wpdb->tables( 'ms_global' ) as $table => $prefixed_table )
+	foreach ( $wpdb->tables( 'ms_global' ) as $table => $prefixed_table ) {
 		$wpdb->$table = $prefixed_table;
+	}
 	install_network();
-	$result = populate_network(1, WP_TESTS_DOMAIN, WP_TESTS_EMAIL, WP_TESTS_NETWORK_TITLE, '/', WP_TESTS_SUBDOMAIN_INSTALL);
+	$result = populate_network( 1, WP_TESTS_DOMAIN, WP_TESTS_EMAIL, WP_TESTS_NETWORK_TITLE, '/', WP_TESTS_SUBDOMAIN_INSTALL );
 
 	system( WP_PHP_BINARY . ' ' . escapeshellarg( dirname( __FILE__ ) . '/ms-install.php' ) . ' ' . escapeshellarg( $config_file_path ) );
 
