72bd668e30aa5c2a4d35f97e9d2bb86c5074ea1f
Julien Liabeuf <julien@liabeuf.fr>
2016-04-07 16:03:18 +0700

    Add fallback for wp_roles() which was introduced in WP 4.3

--- includes/functions-fallback.php
+++ includes/functions-fallback.php
@@ -10,3 +10,23 @@ if( ! function_exists( 'boolval' ) ) {
         return !! $var;
     }
 }
+
+if ( ! function_exists( 'wp_roles' ) ) {
+    /**
+     * Retrieves the global WP_Roles instance and instantiates it if necessary.
+     *
+     * @since 4.3.0
+     *
+     * @global WP_Roles $wp_roles WP_Roles global instance.
+     *
+     * @return WP_Roles WP_Roles global instance if not already instantiated.
+     */
+    function wp_roles() {
+        global $wp_roles;
+        if ( ! isset( $wp_roles ) ) {
+            $wp_roles = new WP_Roles();
+        }
+
+        return $wp_roles;
+    }
+}
