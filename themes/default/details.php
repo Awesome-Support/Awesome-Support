35f56e08f274d8e4dfd8c7c96a4afd2d93b39127
Julien Vernet <julien@vernet.me>
2016-04-12 16:07:11 +0700

    Add logout link (#278) + Ticket list link and open new (#261)

--- themes/default/details.php
+++ themes/default/details.php
@@ -23,6 +23,12 @@ $author = get_user_by( 'id', $post->post_author );
 ?>
 <div class="wpas wpas-ticket-details">
 
+	<div class="wpas-ticket-buttons-top">
+		<?php wpas_make_button( __( 'My Tickets', 'awesome-support' ), array( 'type' => 'link', 'link' => wpas_get_tickets_list_page_url(), 'class' => 'wpas-btn wpas-btn-default wpas-link-ticketlist' ) ); ?>
+		<?php wpas_make_button( __( 'Open a ticket', 'awesome-support' ), array( 'type' => 'link', 'link' => wpas_get_submission_page_url(), 'class' => 'wpas-btn wpas-btn-default wpas-link-ticketnew' ) ); ?>
+		<?php wpas_make_button( __( 'Logout', 'awesome-support' ), array( 'type' => 'link', 'link' => wp_logout_url(), 'class' => 'wpas-btn wpas-btn-default wpas-link-logout' ) ); ?>
+	</div>
+
 	<?php
 	/**
 	 * Display the table header containing the tickets details.
