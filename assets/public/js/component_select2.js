6829326dd2658fece0541ca86d26f505c0c56491
Julien Vernet <julien@vernet.me>
2016-05-18 15:57:58 +0700

    Add basic select2 init. #268

---
+++ assets/public/js/component_select2.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+
+	/**
+	 * jQuery Select2
+	 * http://select2.github.io/select2/
+	 */
+	if (jQuery().select2 && $('select.wpas-select2').length) {
+		$('select.wpas-select2').select2();
+	}
+
+});
