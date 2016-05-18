270d092853d5159f826472994b71d4fce34e6398
Julien Liabeuf <julien@liabeuf.fr>
2016-05-18 15:03:24 +0700

    Enqueue the select2 assets when select2 is enabled - see #268

--- includes/custom-fields/class-custom-fields.php
+++ includes/custom-fields/class-custom-fields.php
@@ -63,6 +63,42 @@ class WPAS_Custom_Fields {
 	}
 
 	/**
+	 * Register and enqueue the select2 assets
+	 *
+	 * This method will be called if the select2 parameter is passed with a custom field (could be a select or a
+	 * taxonomy field).
+	 *
+	 * @since 3.3
+	 * @return void
+	 */
+	public function enqueue_select2_assets() {
+
+		global $post;
+
+		wp_register_style( 'wpas-select2', WPAS_URL . 'assets/admin/css/vendor/select2.min.css', null, '3.5.2', 'all' );
+		wp_register_script( 'wpas-select2', WPAS_URL . 'assets/admin/js/vendor/select2.min.js', array( 'jquery' ), '4.0.0', true );
+
+		$ticket_submit = wpas_get_option( 'ticket_submit' );
+
+		if ( ! is_array( $ticket_submit ) ) {
+			$ticket_submit = (array) $ticket_submit;
+		}
+
+		if ( ! in_array( $post->ID, $ticket_submit ) ) {
+			return;
+		}
+
+		if ( false === wp_style_is( 'wpas-select2', 'enqueued' ) ) {
+			wp_enqueue_style( 'wpas-select2' );
+		}
+
+		if ( false === wp_script_is( 'wpas-select2', 'enqueued' ) ) {
+			wp_enqueue_script( 'wpas-select2' );
+		}
+
+	}
+
+	/**
 	 * Add a new custom field to the ticket.
 	 *
 	 * @param string $name Option name
@@ -111,6 +147,11 @@ class WPAS_Custom_Fields {
 		$option = array( 'name' => $name, 'args' => $arguments );
 
 		$this->options[ $name ] = apply_filters( 'wpas_add_field', $option );
+
+		// If select2 is enabled we load the required assets
+		if ( isset( $arguments['select2'] ) && true === $arguments['select2'] ) {
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_select2_assets' ) );
+		}
 
 		return true;
 
