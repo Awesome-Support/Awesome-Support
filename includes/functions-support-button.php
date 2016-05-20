a74508fd106628d8a6377406aa76f1b6febb7afd
Julien Vernet <julien@vernet.me>
2016-05-20 11:39:38 +0700

    Re-indent code using phpfmt. #233

--- includes/functions-support-button.php
+++ includes/functions-support-button.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'wp_footer', 'wpas_support_btn', 100 );
+add_action('wp_footer', 'wpas_support_btn', 100);
 /**
  * Create a support button that points to the ticket submission page
  *
@@ -8,21 +8,21 @@ add_action( 'wp_footer', 'wpas_support_btn', 100 );
  * @return void
  */
 function wpas_support_btn() {
-	$enable = (bool) wpas_get_option ( 'support_btn_enable' );
-	$logged_in = (bool) wpas_get_option ( 'support_btn_logged_in' );
+	$enable    = (bool) wpas_get_option('support_btn_enable');
+	$logged_in = (bool) wpas_get_option('support_btn_logged_in');
 
-	if ( false === $enable || wpas_is_plugin_page() ) {
+	if (false === $enable || wpas_is_plugin_page()) {
 		return;
 	}
 
-	if( true === $logged_in && false === is_user_logged_in() ) {
-		return;    	
+	if (true === $logged_in && false === is_user_logged_in()) {
+		return;
 	}
 
-	$label = wpas_get_option ( 'support_btn_label' );
-	$color_text = wpas_get_option ( 'support_btn_color_text' );
-	$color_background = wpas_get_option ( 'support_btn_color_background' );
-	$position = wpas_get_option ( 'support_btn_position' );
+	$label              = wpas_get_option('support_btn_label');
+	$color_text         = wpas_get_option('support_btn_color_text');
+	$color_background   = wpas_get_option('support_btn_color_background');
+	$position           = wpas_get_option('support_btn_position');
 	$ticket_submit_page = wpas_get_submission_page_url();
 
 	echo "<a class='wpas_support_btn $position' href='$ticket_submit_page' style='color: $color_text; background-color: $color_background;'>$label</a>";
