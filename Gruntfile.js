9f61516f5012e99c4edd13b1316c9b81dce74bec
Julien Liabeuf <julien@liabeuf.fr>
2016-05-30 17:42:35 +0700

    Remove txpull from release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -266,7 +266,7 @@ module.exports = function (grunt) {
 	grunt.registerTask('txpull', ['exec:txpull', 'potomo']);
 	grunt.registerTask('txpush', ['makepot', 'exec:txpush']);
 
-	grunt.registerTask('release', ['composer:install', 'build', 'txpull', 'compress']);
+	grunt.registerTask('release', ['composer:install', 'build', 'compress']);
 	grunt.registerTask('release_patch', ['version::patch', 'release']);
 	grunt.registerTask('release_minor', ['version::minor', 'release']);
 	grunt.registerTask('release_major', ['version::major', 'release']);
