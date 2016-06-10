5ad81796779169e7624ebea3b10200ef7fecc615
Julien Liabeuf <julien@liabeuf.fr>
2016-06-10 14:18:11 +0700

    Don't forget to echo the user name...

--- includes/admin/functions-user-profile.php
+++ includes/admin/functions-user-profile.php
@@ -51,7 +51,7 @@ function wpas_user_profile_contact_info_contents( $info, $user, $ticket_id ) {
 	switch ( $info ) {
 
 		case 'name':
-			$name = apply_filters( 'wpas_user_profile_contact_name', $user->data->display_name, $user, $ticket_id );
+			echo apply_filters( 'wpas_user_profile_contact_name', $user->data->display_name, $user, $ticket_id );
 			break;
 
 		case 'role':
