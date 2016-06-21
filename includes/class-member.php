97a901dd8f9862b3676a8bfd58d7e9fdd30eb764
Julien Liabeuf <julien@liabeuf.fr>
2016-06-21 10:07:43 +0700

    Typo

--- includes/class-member.php
+++ includes/class-member.php
@@ -180,7 +180,7 @@ class WPAS_Member {
 	/**
 	 * Setup the user capabilities based on their roles
 	 *
-	 * This method is mostly mased on WordPress' WP_User::get_role_caps()
+	 * This method is mostly based on WordPress' WP_User::get_role_caps()
 	 *
 	 * @since 3.3
 	 * @return void
