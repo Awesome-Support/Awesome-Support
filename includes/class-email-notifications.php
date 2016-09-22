b049fdb3304c4c3110c3f08bdcce56d0d393ea88
Robert Kramer <rwkramer.iii@customdesigns.com>
2016-09-21 21:43:42 -0500

    Fixed PR #341 - is_active()

--- includes/class-email-notifications.php
+++ includes/class-email-notifications.php
@@ -151,8 +151,7 @@ class WPAS_Email_Notification {
 		$option = $options[$case];
 
 		/* Replace the valueless tags array by the new one */
-		return boolval( apply_filters( 'wpas_email_notifications_case_is_active', wpas_get_option( $option, false ) ));
-		//return boolval( wpas_get_option( $option, false ) );
+		return (bool) apply_filters( 'wpas_email_notifications_case_is_active', wpas_get_option( $option, false ), $case ); 
 		
 	}
 
