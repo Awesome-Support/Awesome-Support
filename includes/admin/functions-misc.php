afcdcb89decae88beff83740a11788354c12a0d2
Nigel Bahadur <elindydotcom@hotmail.com>
2017-04-18 01:27:02 -0500

    Add request for 5 star rating after the user has closed 25 tickets.

--- includes/admin/functions-misc.php
+++ includes/admin/functions-misc.php
@@ -313,3 +313,41 @@ function wpas_free_addon_notice() {
 	), admin_url( 'edit.php' ) ) ), array( 'strong' => array(), 'a' => array( 'href' => array() ) ) ) );
 
 }
+
+add_action( 'plugins_loaded', 'wpas_request_first_5star_rating' );
+/**
+ * Request 5 star rating after 25 closed tickets.
+ *
+ * After 25 closed tickets we ask the admin for a 5 star rating
+ *
+ * @since 4.0.0
+ * @return void
+ */
+function wpas_request_first_5star_rating() {
+
+	// Only show this message to admins
+	if ( ! current_user_can( 'administrator' ) ) {
+		return;
+	}
+
+	// Only show the notice on the plugin pages
+	if ( ! wpas_is_plugin_page() ) {
+		return;
+	}
+	
+	// If notice has been dismissed, return since everything else after this is expensive operations!
+	If ( wpas_is_notice_dismissed('wpas_request_first_5star_rating') ) {
+		return ;
+	}
+
+	// How many tickets have been closed?
+	$closed_tickets = wpas_get_tickets( 'closed', array( 'posts_per_page' => 25 ) );
+	
+	// Show notice if number of closed tickets greater than 25.
+	If ( count ($closed_tickets) >= 25 ) {
+	
+		WPAS()->admin_notices->add_notice( 'updated', 'wpas_request_first_5star_rating', wp_kses( sprintf( __( 'Wow! It looks like you have closed at least 25 tickets!  Way to go! We guess you must really like Awesome Support, huh? Could you please do us a favor and leave a 5 star rating on WordPress? It will only take a minute and helps to motivate our developers and volunteers. <a href="%1$s">Yes, you deserve it!</a>.', 'awesome-support' ), 'https://wordpress.org/support/plugin/awesome-support/reviews/' ) , 
+		array( 'strong' => array(), 'a' => array( 'href' => array() ) ) ) );
+
+	}
+}
