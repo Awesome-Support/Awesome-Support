74147eb1a5e015449a83884411a5a3c1c79794d4
lubo <npeasy@163.com>
2017-07-11 20:36:29 +0800

    change mkdir to wp_mkdir_p

--- includes/file-uploader/class-file-uploader.php
+++ includes/file-uploader/class-file-uploader.php
@@ -354,7 +354,7 @@ class WPAS_File_Upload {
 	 */
 	public function create_upload_dir( $dir ) {
 
-		$make = mkdir( $dir, 0777, true );
+		$make = wp_mkdir_p ( $dir );
 
 		if ( true === $make ) {
 			$this->protect_upload_dir( $dir );
