582cd9bbc59f9465d79b8369ec1c882670b8a947
Julien Liabeuf <julien@liabeuf.fr>
2017-08-20 19:16:48 +0700

    Add EDD API credentials settings

--- awesome-support.php
+++ awesome-support.php
@@ -450,12 +450,13 @@ if ( ! class_exists( 'Awesome_Support' ) ):
 			require( WPAS_PATH . 'includes/admin/functions-ajax.php' );
 			require( WPAS_PATH . 'includes/admin/functions-log-viewer.php' );
 			require( WPAS_PATH . 'includes/admin/functions-admin-ticket-detail-toolbars.php' );
+			require( WPAS_PATH . 'includes/admin/class-addons-installer.php' );
 			
 			if ( ! class_exists( 'TAV_Remote_Notification_Client' ) ) {
 				if ( ! defined( 'WPAS_REMOTE_NOTIFICATIONS_OFF' ) || true !== WPAS_REMOTE_NOTIFICATIONS_OFF ) {
 					require( WPAS_PATH . 'includes/class-remote-notification-client.php' );
 				}
-			}			
+			}
 
 			// We don't need all this during Ajax processing
 			if ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) {
@@ -484,6 +485,7 @@ if ( ! class_exists( 'Awesome_Support' ) ):
 				require( WPAS_PATH . 'includes/admin/settings/settings-notifications.php' );
 				require( WPAS_PATH . 'includes/admin/settings/settings-advanced.php' );
 				require( WPAS_PATH . 'includes/admin/settings/settings-licenses.php' );
+				require( WPAS_PATH . 'includes/admin/settings/settings-addons.php' );
 				require( WPAS_PATH . 'includes/admin/settings/settings-products-management.php' );
 				require( WPAS_PATH . 'includes/admin/settings/settings-basic-time-tracking.php' );
 				require( WPAS_PATH . 'includes/admin/settings/settings-integration.php' );
