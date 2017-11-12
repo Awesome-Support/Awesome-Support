8cf99b3eb2a9c60b7ebce3b95ba6d6f5ca3b15f1
Tahir Nazir <sunaurmoon@gmail.com>
2017-11-12 05:28:39 +0000

    Add tabs to AS Ticket ABOUT Page

---
+++ includes/admin/views/about-tab-change-log.php
@@ -0,0 +1,191 @@
+<div class="about-text">Trusted by over 6000+ Happy Users, Awesome Support is the most versatile WordPress support plugin. So now, lets get you started!</div>
+	
+	<hr />	
+	
+	<div>
+
+		<h2>Quick Start With The Basics</h2>
+		<ol>
+			<li>Your ticket system is all ready to go so getting started is really simple!  A new page has been created named SUBMIT TICKET. Just add it to your WordPress menu and your customers can start submitting tickets on your site!</li>
+			<li>Our videos page has a <a href= "https://www.getawesomesupport.com/videos"> quick-start and setup video</a> - use it to follow along and quickly configure your new help desk.</li>
+			<li>Here are links to our very extensive documentation: <a href= "https://getawesomesupport.com/documentation/awesome-support/admin-overview/"> Admin Documentation </a> &nbsp <a href= "https://getawesomesupport.com/documentation/awesome-support/post-installation-need-know-quick-start/"> User Documentation </a> </li>					
+		</ol>
+		
+		<h2>Tickets, Agents and Users</h2>
+		<ol>
+			<li>You can find a list of new and existing tickets under the TICKETS menu option - this is your main dashboard for managing tickets.</li>
+			<li><b>Set up new agents:</b> Your primary WordPress Admin is now an agent and will automatically be assigned all new tickets unless you <a href= "https://getawesomesupport.com/documentation/awesome-support/basic-admin-add-agent/"> set up additional agents.</a></li>
+			<li><b>Existing Users:</b> If you have existing users you will need to update their roles to be <b>Support User</b>. Or update their existing roles to <a href="https://getawesomesupport.com/documentation/awesome-support/how-to-fix-you-do-not-have-the-capacity-to-open-a-new-ticket/">add the proper WordPress security capabilities for Awesome Support ticket users. </a>  This will grant them the permissions necessary to submit tickets.</li>
+			<li>Your users can find a list of their tickets on the MY TICKETS page - you just need to add that page to your WordPress menu as well.</li>
+		</ol>			
+	</div>
+
+	<hr />	
+	
+	<div class="changelog">
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="about-body">
+					<a class="wpas-bundle-link" href="https://getawesomesupport.com/addons/startup-bundle/" target="_blank">
+						<img src="<?php echo WPAS_URL; ?>assets/admin/images/StartupBundle-2_Blue.png" alt="Startup Bundle">	
+					</a>		
+				</div>
+			</div>				
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">				
+				<div class="about-body">
+					<a class="wpas-bundle-link" href="https://getawesomesupport.com/addons/professional-bundle/" target="_blank">
+						<img src="<?php echo WPAS_URL; ?>assets/admin/images/ProfessionalBundle-1_Red.png" alt="Professsional Bundle">	
+					</a>		
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div>			
+				<div class="about-body">
+					<h1>What's New In 4.0</h1>
+					<h2>Improved Ticket List And Ticket Management</h2>
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/AS-V400-WhatsNew-TicketList-01.png" alt="Improved Ticket List">					
+					<p>Version 4 introduces <b>pre-configured custom fields</b> for tracking Priorities, Channels (the source of your tickets), additional agents, third parties and more!</a>.</p>
+					<p><b>Sorting and filtering</b> options are substantially improved.  Now, 95% of the columns on the screen can be used for sorting and filtering.</p>
+					<p>It is easier than ever to manage large volumes of tickets - Awesome Support now scales from a single person operation to the largest enterprises.</p>
+					<p><b>Assign up to two additional agents</b> to each ticket without losing the flexibility to transfer tickets to any agent.</p>
+				</div>
+				
+				<div class="about-body">
+					<h2>NEW: Time Tracking</h2>
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/AS-V400-WhatsNew-TimeTracking-01.png" alt="New - Basic Time Tracking">					
+					<p><b>Basic time tracking</b> at the ticket level is now available. Agents can enter the total time they spent on a ticket along with any adjustments and time related notes.</a>.</p>
+					<p>Time columns can optionally be added to the primary ticket list view.</p>
+					<p>Advanced automatic and manual time-tracking down to the reply level is supported via add-ons.</p>
+					<p><a href="https://getawesomesupport.com/documentation/awesome-support/admin-basic-time-tracking/" target="_blank">Read the documentation</a></p>
+				</div>				
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="about-body">
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/v400/custom_field_enhancement_green.jpg" alt="Custom Fields Enhancements">
+					<h3>Custom Fields Enhancements</h3>
+					<p><b>New:</b> Admin Only Fields (Finally!)</p>
+					<p><b>New:</b> Read Only Fields.</p>
+					<p><b>New:</b> Control over where fields show up in the ticket in the admin panel.</p>
+					<p><b>New:</b> Control over how the fields are displayed.</p>
+					<p><b>New:</b> Custom taxonomy fields now have options for individual <em>capabilities</em>.</p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="about-body">
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/v400/Security.jpg" alt="Security Enhancements">
+					<h3>Security Enhancements</h3>
+					<p><b>New:</b> 19 additional WordPress capabilities.</p>
+					<p><b>New:</b> Granular control over who can view all tickets.</p>
+					<p><b>New:</b> Granular control over who can view unassigned tickets created by the email add-on.</p>
+					<p><b>New:</b> Granular control over who can view your license information in the license tab. </p>
+					<p><b>New:</b> Granular control over all pre-installed taxonomies such as priorities, products and departments. </p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="about-body">
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/v400/numerous_smaller_enhancements.jpg" alt="Numerous smaller Enhancements">
+					<h3>Numerous Smaller Enhancements</h3>
+					<p><b>New:</b> Additional options for the ticket URL/SLUG </p>
+					<p><b>New:</b> Add ticket ID column to front end ticket list .</p>
+					<p><b>New:</b> New options to control where an agent ends up after replying to a ticket (next ticket, previous ticket).</p>
+					<p><b>New:</b> Additional action and filter hooks for developers.</p>					
+					<p> Many more! <a href="https://getawesomesupport.com/major-core-update-4-0-0/" target="_blank">Read the entire change log</a>.</p>	
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="about-body">
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/v400/fixes.jpg" alt="Multiple bug fixes">
+					<h3>And, Of Course, Multiple Bug Fixes</h3>
+					<p>Clean up some PHP notices.</p>
+					<p>When the agent selected "Back to List" for their "after reply" action in their personal profile, this action did not work.</p>
+					<p>Updated default styles on checkbox and radio button custom fields.</p>
+					<p>Product syncing is now more reliable.</p>
+					<p> Many more! <a href="https://getawesomesupport.com/major-core-update-4-0-0/" target="_blank">Read the entire change log</a>.</p>	
+				</div>
+			</div>
+		</div>				
+		
+		<div class="row">
+			<div>			
+				<div class="about-body">
+					<h2>Release Notes For Version 3.0</h2>
+				</div>
+			</div>
+		</div>				
+		
+		
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="about-body">
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/about-cf.png" alt="Improved Custom Fields">
+					<h3>Improved Custom Fields</h3>
+                    <p>This feature lets your tailor your support system to your needs, allowing you to <mark>add more fields to the ticket submission form</mark>.</p>
+					<p>Since version 3.1.2, custom fields become much more powerful. They are simpler to use, and many more field types are available: checkbox, email, number, radio, text, and much more. Find all the available field types in <a href="https://getawesomesupport.com/documentation/awesome-support/custom-fields/" target="_blank">the documentation</a>.</p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="about-body">
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/about-autoassign.png" alt="Auto-Assignment">
+					<h3>Auto-Assignment</h3>
+					<p>Awesome Support comes with an "intelligent" ticket assignment system. New tickets are automatically assigned to the agent with the least open tickets.</p>
+					<p>But you can also <mark>enable/disable the auto-assignation for specific users</mark> (for instance a site administrator).</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="about-body">
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/about-multipleforms.png" alt="Multiple Submission Forms">
+					<h3>Multiple Submission Forms</h3>
+					<p>You can now create multiple submission forms for your users and pre-set values for each field. Need a for for your technical support? Create a new one and link it with the correct URL parameter to pre-populate the "type" field. More in the documentation.</p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="about-body">
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/about-translations.png" alt="Translated in many languages">
+					<h3>Translated in many languages</h3>
+					<p>Thanks to our contributors, the plugin is available in several languages. Currently the plugin is <mark>available in nearly 10 languages</mark>.</p>
+					<p>To check out available translations, please visit <a href="https://getawesomesupport.com/documentation/awesome-support/translations/" target="_blank">our POEditor.com project list</a>.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row row-wpas-more">
+			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+				<div class="about-body">
+					<h2>Newsfeed</h2>
+					<div class="wpas-fbpage-feed">Loading...</div>
+					<p><a class="button button-large button-primary" href="https://www.facebook.com/awesomesupport" target="_blank">View more news</a></p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+				<div class="about-body">
+					<h2>Contributing</h2>
+					<h4>Open Source</h4>
+					<p>The code is open source and <a href="https://github.com/ThemeAvenue/Awesome-Support" target="_blank">available on GitHub</a> for anyone to contribute. Even you.</p>
+					<h4>Translation Ready</h4>
+					<p>The plugin is fully localized. You can <a href="https://poeditor.com/join/project/P6HgfPnBt4" target="_blank">translate the plugin</a> in any language!</p>
+					<h4>Rate the plugin</h4>
+					<p>If you like the plugin, make sure to rate it on the WordPres extend. This is perhaps one of the best way to share the love for our plugin <a href="https://wordpress.org/support/view/plugin-reviews/awesome-support?rate=5#postform" target="_blank">Rate the plugin →</a></p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+				<div class="about-body">
+					<h2>Extending the Possibilities</h2>
+					<p>Even though Awesome Support has a lot of built-in features, it is impossible to make everyone happy. This is why we have lots of addons to help you tailor your support system.</p>
+					<a href="<?php echo esc_url( add_query_arg( array( 'post_type' => 'ticket', 'page' => 'wpas-addons' ), admin_url( 'edit.php' ) ) ); ?>" class="button button-large button-primary">Browse extensions</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
---
+++ includes/admin/views/about-tab-getting-started.php
@@ -0,0 +1,22 @@
+<div class="row">
+			<div>			
+				<div class="about-body">
+					<h1>What's New In 4.0</h1>
+					<h2>Improved Ticket List And Ticket Management</h2>
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/AS-V400-WhatsNew-TicketList-01.png" alt="Improved Ticket List">					
+					<p>Version 4 introduces <b>pre-configured custom fields</b> for tracking Priorities, Channels (the source of your tickets), additional agents, third parties and more!</a>.</p>
+					<p><b>Sorting and filtering</b> options are substantially improved.  Now, 95% of the columns on the screen can be used for sorting and filtering.</p>
+					<p>It is easier than ever to manage large volumes of tickets - Awesome Support now scales from a single person operation to the largest enterprises.</p>
+					<p><b>Assign up to two additional agents</b> to each ticket without losing the flexibility to transfer tickets to any agent.</p>
+				</div>
+				
+				<div class="about-body">
+					<h2>NEW: Time Tracking</h2>
+					<img src="<?php echo WPAS_URL; ?>assets/admin/images/AS-V400-WhatsNew-TimeTracking-01.png" alt="New - Basic Time Tracking">					
+					<p><b>Basic time tracking</b> at the ticket level is now available. Agents can enter the total time they spent on a ticket along with any adjustments and time related notes.</a>.</p>
+					<p>Time columns can optionally be added to the primary ticket list view.</p>
+					<p>Advanced automatic and manual time-tracking down to the reply level is supported via add-ons.</p>
+					<p><a href="https://getawesomesupport.com/documentation/awesome-support/admin-basic-time-tracking/" target="_blank">Read the documentation</a></p>
+				</div>				
+			</div>
+		</div>
