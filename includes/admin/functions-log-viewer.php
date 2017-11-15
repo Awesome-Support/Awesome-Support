a284fc4358c171e95956ce9c3a1b5a2e6cf13b81
Robert Kramer <rwkramer.iii@customdesigns.com>
2017-11-15 16:45:52 -0600

    misc: Trivial code documentation and cleanup.

--- includes/admin/functions-log-viewer.php
+++ includes/admin/functions-log-viewer.php
@@ -1,19 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Robert
- * Date: 6/5/2017
- * Time: 8:34 PM
+ * Templating Functions.
+ *
+ * This file contains all the log viewer functions and AJAX Handlers.
+ *
+ * @package   Awesome_Support
+ * @author    Robert W. Kramer III
+ * @license   GPL-2.0+
+ * @link      https://getawesomesupport.com
+ * @copyright 2014-2017 AwesomeSupport
  */
 
-/*
-log viewer - just a couple of minor changes:
-1. Don't show the logs folder or show it at the bottom of the screen.
-2. Make the main background color gray and the viewer itself black text on white background.
-3. Remove the horizontal lines across the top - looks too busy with them there. This is a simple interface so lets make it less busy by doing that.
-4. If possible, add an option to turn off wordwrap.
-
-*/
 
 /*
  * Actions
@@ -24,12 +21,22 @@ log viewer - just a couple of minor changes:
  *
  */
 
+/**
+ * AS Logs Path
+ *
+ * @return string
+ */
 function get_logs_path() {
-	return WPAS_PATH  . 'logs/';
+	return WPAS_PATH . 'logs/';
 }
 
+/**
+ * AS Logs URL
+ *
+ * @return string
+ */
 function get_logs_url() {
-	return WPAS_URL  . 'logs/';
+	return WPAS_URL . 'logs/';
 }
 
 /*
@@ -43,7 +50,7 @@ function wpas_tools_log_viewer_view() {
 
 	check_ajax_referer( 'wpas_tools_log_viewer_view', 'nonce' );
 
-	if( ! isset( $_POST[ 'file' ] ) ) {
+	if ( ! isset( $_POST[ 'file' ] ) ) {
 		wp_send_json_error( array( 'error' => esc_html__( 'No file given', 'awesome-support' ) ) );
 	}
 
@@ -52,13 +59,14 @@ function wpas_tools_log_viewer_view() {
 	$file  = basename( $_POST[ 'file' ] );
 
 	// Get posted number of lines
-	if( isset( $_POST[ 'lines' ] ) ) {
+	if ( isset( $_POST[ 'lines' ] ) ) {
 		$lines = $_POST[ 'lines' ];
 	}
 
 	wp_send_json_success( wpas_log_viewer_read_last_lines( $file, $lines ) );
 
 }
+
 add_action( 'wp_ajax_wpas_tools_log_viewer_view', 'wpas_tools_log_viewer_view', 10, 0 );
 
 
@@ -73,7 +81,7 @@ function wpas_tools_log_viewer_download() {
 
 	check_ajax_referer( 'wpas_tools_log_viewer_download', 'nonce' );
 
-	if( ! isset( $_POST[ 'file' ] ) ) {
+	if ( ! isset( $_POST[ 'file' ] ) ) {
 		wp_send_json_error( array( 'error' => esc_html__( 'No file given', 'awesome-support' ) ) );
 	}
 
@@ -81,14 +89,15 @@ function wpas_tools_log_viewer_download() {
 
 	$content = array(
 		'status' => array(
-			'code' => '200',
-		    'message' => 'Downloading...'
+			'code'    => '200',
+			'message' => 'Downloading...',
 		),
-		'url' => get_logs_url() . $file
+		'url'    => get_logs_url() . $file,
 	);
 
 	wp_send_json_success( $content );
 }
+
 add_action( 'wp_ajax_wpas_tools_log_viewer_download', 'wpas_tools_log_viewer_download', 10, 0 );
 
 
@@ -103,16 +112,17 @@ function wpas_tools_log_viewer_delete() {
 
 	check_ajax_referer( 'wpas_tools_log_viewer_delete', 'nonce' );
 
-	if( ! isset( $_POST[ 'file' ] ) ) {
+	if ( ! isset( $_POST[ 'file' ] ) ) {
 		echo json_encode( array( 'error' => esc_html__( 'No file given', 'awesome-support' ) ) );
 		wp_die();
 	}
 
 	$file = basename( $_POST[ 'file' ] );
 
-	wp_send_json_success(	wpas_log_viewer_delete_file( $file ) );
+	wp_send_json_success( wpas_log_viewer_delete_file( $file ) );
 
 }
+
 add_action( 'wp_ajax_wpas_tools_log_viewer_delete', 'wpas_tools_log_viewer_delete', 10, 0 );
 
 
@@ -121,21 +131,20 @@ add_action( 'wp_ajax_wpas_tools_log_viewer_delete', 'wpas_tools_log_viewer_delet
  */
 function wpas_log_viewer_delete_file( $file ) {
 
-	if( unlink( get_logs_path() . $file ) ) {
-		$code = '200';
+	if ( unlink( get_logs_path() . $file ) ) {
+		$code    = '200';
 		$content = "Deleted " . $file . " successfully.";
-	}
-	else {
-		$code = '404';
+	} else {
+		$code    = '404';
 		$content = "Delete " . $file . " failed.";
 	}
 
 	$json = array(
 		'status' => array(
-			'code' => $code,
-		    'message' => $content
+			'code'    => $code,
+			'message' => $content,
 		),
-		'url' => get_logs_url() . $file
+		'url'    => get_logs_url() . $file,
 	);
 
 	wp_send_json_success( $json );
@@ -146,15 +155,15 @@ function wpas_log_viewer_delete_file( $file ) {
 /* Function read X last lines from file*/
 function wpas_log_viewer_read_last_lines( $file, $lines ) {
 
-	if( 'All' === $lines ) {
+	if ( 'All' === $lines ) {
 		return wpas_log_viewer_read_full_file( $file );
 	}
 
-	$result = [];
-	$file_path   = get_logs_path() . $file;
+	$result    = [];
+	$file_path = get_logs_path() . $file;
 
 	$handle = @fopen( $file_path, "r" );
-	if( ! empty( $handle ) ) {
+	if ( ! empty( $handle ) ) {
 		$linecounter = $lines;
 		$pos         = - 2;
 		$beginning   = false;
@@ -162,7 +171,7 @@ function wpas_log_viewer_read_last_lines( $file, $lines ) {
 		while ( $linecounter > 0 ) {
 			$t = "";
 			while ( $t != "\n" ) {
-				if( fseek( $handle, $pos, SEEK_END ) == - 1 ) {
+				if ( fseek( $handle, $pos, SEEK_END ) == - 1 ) {
 					$beginning = true;
 					break;
 				}
@@ -170,38 +179,37 @@ function wpas_log_viewer_read_last_lines( $file, $lines ) {
 				$pos --;
 			}
 			$linecounter --;
-			if( $beginning ) {
+			if ( $beginning ) {
 				//rewind( $handle );
 			}
 			$text[ $lines - $linecounter - 1 ] = fgets( $handle );
-			if( $beginning ) {
+			if ( $beginning ) {
 				break;
 			}
 		}
 		fclose( $handle );
-		foreach( $text as $line ) {
+		foreach ( $text as $line ) {
 			$result[] = $line;
 		}
 
 		return array(
-			'status' => array(
+			'status'   => array(
 				'code'    => '200',
-				'message' => sprintf(__( "Read %d lines from %s", 'awesome-support' ), count($result), esc_html( $file )),
+				'message' => sprintf( __( "Read %d lines from %s", 'awesome-support' ), count( $result ), esc_html( $file ) ),
 			),
 			'fileinfo' => array(
-				'created' => date ("F d Y H:i:s", filectime($file_path)),
-				'lastmodified' => date ("F d Y H:i:s", filemtime($file_path)),
-			    'filesize' => formatbytes(filesize($file_path)),
+				'created'      => date( "F d Y H:i:s", filectime( $file_path ) ),
+				'lastmodified' => date( "F d Y H:i:s", filemtime( $file_path ) ),
+				'filesize'     => formatbytes( filesize( $file_path ) ),
 			),
-			'data'   => $result,
+			'data'     => $result,
 		);
-	}
-	else {
+	} else {
 		//return printf( __( "Couldn't open the file %s. Make sure file is exists or is readable.", 'error-log-viewer' ), esc_html( $file ) );
 		return array(
 			'status' => array(
 				'code'    => '404',
-				'message' => sprintf(__( "Couldn't open the file %s. Make sure the file exists and is readable.", 'awesome-support' ), esc_html( $file )),
+				'message' => sprintf( __( "Couldn't open the file %s. Make sure the file exists and is readable.", 'awesome-support' ), esc_html( $file ) ),
 			),
 			'data'   => [],
 		);
@@ -217,7 +225,7 @@ function wpas_log_viewer_read_full_file( $file ) {
 
 	$handle = @fopen( $file_path, 'r' );
 	$result = [];
-	if( ! empty( $handle ) ) {
+	if ( ! empty( $handle ) ) {
 		while ( ! feof( $handle ) ) {
 			$line     = fgets( $handle );
 			$result[] = $line;
@@ -225,57 +233,79 @@ function wpas_log_viewer_read_full_file( $file ) {
 		fclose( $handle );
 
 		return array(
-			'status' => array(
+			'status'   => array(
 				'code'    => '200',
 				//'message' => '',
-				'message' => sprintf(__( "Read %d lines from %s", 'awesome-support' ), count($result), esc_html( $file )),
+				'message' => sprintf( __( "Read %d lines from %s", 'awesome-support' ), count( $result ), esc_html( $file ) ),
 			),
 			'fileinfo' => array(
-				'created' => date ("F d Y H:i:s", filectime($file_path)),
-				'lastmodified' => date ("F d Y H:i:s", filemtime($file_path)),
-			    'filesize' => formatbytes(filesize($file_path)),
+				'created'      => date( "F d Y H:i:s", filectime( $file_path ) ),
+				'lastmodified' => date( "F d Y H:i:s", filemtime( $file_path ) ),
+				'filesize'     => formatbytes( filesize( $file_path ) ),
 			),
-			'data'   => $result,
+			'data'     => $result,
 		);
-	}
-	else {
+	} else {
 		//return printf( __( "Couldn't open the file %s. Make sure file is exists or is readable.", 'error-log-viewer' ), esc_html( $file ) );
 		return array(
 			'status' => array(
 				'code'    => '404',
-				'message' => sprintf(__( "Couldn't open the file %s. Make sure the file exists and is readable.", 'awesome-support' ), esc_html( $file )),
+				'message' => sprintf( __( "Couldn't open the file %s. Make sure the file exists and is readable.", 'awesome-support' ), esc_html( $file ) ),
 			),
 			'data'   => [],
 		);
 	}
 }
 
-function formatbytes($val, $digits = 3, $mode = "SI", $bB = "B")
-{
-  $si = array("", "k", "M", "G", "T", "P", "E", "Z", "Y");
-  $iec = array("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi", "Yi");
 
-  switch(strtoupper($mode))
-  {
-      case "SI" : $factor = 1000; $symbols = $si; break;
-      case "IEC" : $factor = 1024; $symbols = $iec; break;
-      default : $factor = 1000; $symbols = $si; break;
-  }
+/**
+ * Read contents of entire file
+ *
+ * @param        $val
+ * @param int    $digits
+ * @param string $mode
+ * @param string $bB
+ *
+ * @return string
+ */
+function formatbytes( $val, $digits = 3, $mode = "SI", $bB = "B" ) {
+	$si  = array( "", "k", "M", "G", "T", "P", "E", "Z", "Y" );
+	$iec = array( "", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi", "Yi" );
 
-  switch($bB)
-  {
-      case "b" : $val *= 8; break;
-      default : $bB = "B"; break;
-  }
+	switch ( strtoupper( $mode ) ) {
+		case "SI" :
+			$factor  = 1000;
+			$symbols = $si;
+			break;
+		case "IEC" :
+			$factor  = 1024;
+			$symbols = $iec;
+			break;
+		default :
+			$factor  = 1000;
+			$symbols = $si;
+			break;
+	}
 
-  for($i=0;$i<count($symbols)-1 && $val>=$factor;$i++)
-      $val /= $factor;
+	switch ( $bB ) {
+		case "b" :
+			$val *= 8;
+			break;
+		default :
+			$bB = "B";
+			break;
+	}
 
-  $p = strpos($val, ".");
-  if($p !== false && $p > $digits)
-    $val = round($val);
-  elseif($p !== false)
-    $val = round($val, $digits-$p);
+	for ( $i = 0; $i < count( $symbols ) - 1 && $val >= $factor; $i ++ ) {
+		$val /= $factor;
+	}
 
-  return round($val, $digits) . " " . $symbols[$i] . $bB;
+	$p = strpos( $val, "." );
+	if ( $p !== false && $p > $digits ) {
+		$val = round( $val );
+	} elseif ( $p !== false ) {
+		$val = round( $val, $digits - $p );
+	}
+
+	return round( $val, $digits ) . " " . $symbols[ $i ] . $bB;
 }
