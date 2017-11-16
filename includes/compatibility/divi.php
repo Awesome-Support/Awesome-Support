d1736171e711b01395b84a1597527f2d589554e0
Nigel Bahadur <elindydotcom@hotmail.com>
2017-11-16 04:48:13 -0600

    DIVI Theme compatibility layer needed some more tweaking.

--- includes/compatibility/divi.php
+++ includes/compatibility/divi.php
@@ -13,23 +13,21 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /* DIVI somehow causes the autosave to activate on our ticket screen so deactivate it. */
-function add_query(){
+function as_divi_add_query(){
 	if ( function_exists( 'et_divi_fonts_url' ) ) {	
-		global $post, $wpdb;
-		if(get_post_type($post->ID) === 'ticket'){
+		if( 'ticket' === get_post_type() ){
 			wp_dequeue_script('autosave');
 		}
 	}
 }
-add_action('admin_print_scripts','add_query');
+add_action('admin_print_scripts','as_divi_add_query');
 
 /* DIVI will pop up a message when the admin creates a new ticket asking to confirm leaving the new ticket screen. 	*/
 /* This change unhooks all form exit scripts when leaving the ticket screen 										*/
 /* Its not perfect since it could cause anything else that needs this hook to fail as well. 						*/
-function add_admin_scripts( $hook ) {
+function as_divi_add_admin_scripts( $hook ) {
 	if ( function_exists( 'et_divi_fonts_url' ) ) {	
-		global $post;
-		if ( 'ticket' === $post->post_type ) {     
+		if ( 'ticket' === get_post_type() ) {     
 			?>
 			<script>
 				jQuery('form#post').submit(function() {
@@ -40,4 +38,4 @@ function add_admin_scripts( $hook ) {
 		}
 	}
 }
-add_action( 'in_admin_footer', 'add_admin_scripts', 10, 1 );
+add_action( 'in_admin_footer', 'as_divi_add_admin_scripts', 10, 1 );
