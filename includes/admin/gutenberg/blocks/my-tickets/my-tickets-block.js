90fcaa2b6bd5fdb3757939f160b300c80eb1fb8b
Nigel Bahadur <elindydotcom@hotmail.com>
2017-12-30 15:00:41 -0600

    Gutenberg Blocks: Added support for internationalization/translation

--- includes/admin/gutenberg/blocks/my-tickets/my-tickets-block.js
+++ includes/admin/gutenberg/blocks/my-tickets/my-tickets-block.js
@@ -7,15 +7,29 @@ var el = wp.element.createElement,
     blockStyle = { backgroundColor: '#900', color: '#fff', padding: '20px' };
 	blockStyleSave = { } ;
 
+var __ = wp.i18n.__; // The __() for internationalization.	
+
+/**
+ * Register Basic Block.
+ *
+ * Registers a new block providing a unique name and an object defining its
+ * behavior. Once registered, the block is made available as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */		
 registerBlockType( 'awesome-support/my-tickets', {
-    title: 'My Tickets',
+    title: __( 'My Tickets', 'awesome-support' ),
 
     icon: 'schedule',
 
     category: 'widgets',
 
     edit: function() {
-        return el( 'p', { style: blockStyle }, 'Awesome Support: My Tickets' );
+        return el( 'p', { style: blockStyle }, __( 'Awesome Support: My Tickets', 'awesome-support' ) );
     },
 
     save: function() {
--- includes/admin/gutenberg/blocks/submit-ticket/submit-ticket-block.js
+++ includes/admin/gutenberg/blocks/submit-ticket/submit-ticket-block.js
@@ -6,16 +6,30 @@ var el = wp.element.createElement,
     registerBlockType = wp.blocks.registerBlockType,
     blockStyle = { backgroundColor: '#900', color: '#fff', padding: '20px' };
 	blockStyleSave = { } ;
+	
+var __ = wp.i18n.__; // The __() for internationalization.	
 
+/**
+ * Register Basic Block.
+ *
+ * Registers a new block providing a unique name and an object defining its
+ * behavior. Once registered, the block is made available as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */	
 registerBlockType( 'awesome-support/submit-ticket', {
-    title: 'Submit Ticket',
+    title: __( 'Submit Ticket', 'awesome-support' ) ,
 
     icon: 'forms',
 
     category: 'widgets',
 
     edit: function() {
-        return el( 'p', { style: blockStyle }, 'Awesome Support: Submit Ticket' );
+        return el( 'p', { style: blockStyle }, __( 'Awesome Support: Submit Ticket', 'awesome-support' ) );
     },
 
     save: function() {
