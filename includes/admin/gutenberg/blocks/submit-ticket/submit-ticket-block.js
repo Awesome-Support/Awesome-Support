81ab704c136e91ddb030df018b26a0f516e9a112
Nigel Bahadur <elindydotcom@hotmail.com>
2017-12-30 15:23:20 -0600

    Gutenberg Blocks: Better code isolation so that minimization and concatenation of JS files don't result in blocks stepping on each other.

--- includes/admin/gutenberg/blocks/submit-ticket/submit-ticket-block.js
+++ includes/admin/gutenberg/blocks/submit-ticket/submit-ticket-block.js
@@ -1,38 +1,41 @@
 /**
  * The submit-ticket shortcode GUTENBERG block
  */
-
-var el = wp.element.createElement,
-    registerBlockType = wp.blocks.registerBlockType,
-    blockStyle = { backgroundColor: '#900', color: '#fff', padding: '20px' };
-	blockStyleSave = { } ;
+( function() {
 	
-var __ = wp.i18n.__; // The __() for internationalization.	
+	var el = wp.element.createElement,
+		registerBlockType = wp.blocks.registerBlockType,
+		blockStyle = { backgroundColor: '#900', color: '#fff', padding: '20px' };
+		blockStyleSave = { } ;
+		
+	var __ = wp.i18n.__; // The __() for internationalization.	
 
-/**
- * Register Basic Block.
- *
- * Registers a new block providing a unique name and an object defining its
- * behavior. Once registered, the block is made available as an option to any
- * editor interface where blocks are implemented.
- *
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */	
-registerBlockType( 'awesome-support/submit-ticket', {
-    title: __( 'Submit Ticket', 'awesome-support' ) ,
+	/**
+	 * Register Basic Block.
+	 *
+	 * Registers a new block providing a unique name and an object defining its
+	 * behavior. Once registered, the block is made available as an option to any
+	 * editor interface where blocks are implemented.
+	 *
+	 * @param  {string}   name     Block name.
+	 * @param  {Object}   settings Block settings.
+	 * @return {?WPBlock}          The block, if it has been successfully
+	 *                             registered; otherwise `undefined`.
+	 */	
+	registerBlockType( 'awesome-support/submit-ticket', {
+		title: __( 'Submit Ticket', 'awesome-support' ) ,
 
-    icon: 'forms',
+		icon: 'forms',
 
-    category: 'widgets',
+		category: 'widgets',
 
-    edit: function() {
-        return el( 'p', { style: blockStyle }, __( 'Awesome Support: Submit Ticket', 'awesome-support' ) );
-    },
+		edit: function( props ) {
+			return el( 'p', { style: blockStyle }, __( 'Awesome Support: Submit Ticket', 'awesome-support' ) );
+		},
 
-    save: function() {
-        return el( 'p', { style: blockStyleSave }, '[ticket-submit]' );
-    },
-} );
+		save: function( props ) {
+			return el( 'p', { style: blockStyleSave }, '[ticket-submit]' );
+		},
+	} );
+	
+})();
