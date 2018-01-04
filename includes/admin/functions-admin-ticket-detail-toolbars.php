bb97b4ae94140eff7f2f85bc723ff53eb3ec3837
Nigel Bahadur <elindydotcom@hotmail.com>
2018-01-04 01:28:20 -0600

    Make the ADD TOOLBAR item button default to returning the markup string instead of echoing it. Make the same function available from global areas in the application by moving it to another file. Modified other functions and files affected by this change.

--- includes/admin/functions-admin-ticket-detail-toolbars.php
+++ includes/admin/functions-admin-ticket-detail-toolbars.php
@@ -49,12 +49,79 @@ function wpas_add_close_ticket_item_to_ticket_detail_toolbar( $post ) {
 	$action = get_ticket_details_action_link( $post );
 	
 	if ( 'closed' === $ticket_status ) {
-		wpas_add_ticket_detail_toolbar_item( 'a', 'wpas-close-ticket-top', __( 'Re-open Ticket', 'awesome-support' ), WPAS_URL . "assets/admin/images/icons/re-open-ticket.png", $action );
+		echo wpas_add_ticket_detail_toolbar_item( 'a', 'wpas-close-ticket-top', __( 'Re-open Ticket', 'awesome-support' ), WPAS_URL . "assets/admin/images/icons/re-open-ticket.png", $action );
 	} elseif( '' === $ticket_status ) {
 		// do nothing...
 	} else {
-		wpas_add_ticket_detail_toolbar_item( 'a', 'wpas-close-ticket-top', __( 'Close Ticket', 'awesome-support' ), WPAS_URL . "assets/admin/images/icons/close-ticket.png", $action );
+		echo wpas_add_ticket_detail_toolbar_item( 'a', 'wpas-close-ticket-top', __( 'Close Ticket', 'awesome-support' ), WPAS_URL . "assets/admin/images/icons/close-ticket.png", $action );
 	}	
+}
+
+/**
+ * Adds an item to the tool-bar in the ticket detail in wp-admin or
+ * returns a markup suitable for adding an item to the toolbar screen.
+ * 
+ * Note that if you choose to have the menu item echoed directly to the screen this
+ * function should be called using a do_action hook tied into the menu (such as wpas_ticket_detail_toolbar01_before)
+ *
+ * @since 4.4.0
+ *
+ * @param 	string $html_element_type   img or a (anchor)
+ *          string $item_css_id 		The CSS ID of the toolbar item 
+ *                                      Any "-" in the name will be converted to underscores to create a class name; the "-" will remain for the CSS ID;
+ *                                      For example: my-css-id will result in a markup with the css id = 'my-css-id' and the class name = 'my_css_id'.
+ *		  	string $tool_tip_text 		The tool tip that will be displayed for the new toolbar item
+ *			string $image_url			The URL for the toolbar item image.
+ *			string $target_url			(optional) The URL target if creating an 'a' (anchor) element
+ *			string $attributes			(optional) Any other attribute that needs to be added to the markup
+ * 			bool   $return_markup		If set to true, it will return the button markup instead of echoing it to the screen.
+ *
+ * @return 	void (basically nothing is returned)
+ */
+function wpas_add_ticket_detail_toolbar_item( $html_element_type, $item_css_id, $tool_tip_text, $image_url, $target_url='', $attributes = '', $return_markup = true ) {
+	
+	$name = str_replace( '-', '_', $item_css_id );  // convert the passed ids into text to be used for classnames.  For convention we're using classnames with underscores (_) and ids with dashes (-)
+
+	$echoout = '';
+	$echoout = $echoout . '<span data-hint=' . '"' . $tool_tip_text . '"' . 'class="wpas-replies-middle-toolbar-item hint-bottom hint-anim">';
+	$echoout = $echoout . ' ' . '<' . $html_element_type . ' ' ;  		// opening tag such as <a> or <img>
+	$echoout = $echoout . ' ' . 'name = ' . '"' . $name . '"' ; 		// name attribute
+	$echoout = $echoout . ' ' . 'id = ' . '"' . $item_css_id . '"' ;	// css ID
+	$echoout = $echoout . ' ' . 'class="link-primary wpas-link-reply wpas-middle-toolbar-links ' . $name . '"' ; // css class names
+	$echoout = $echoout . ' ' . 'value = ' . '"' . $name . '"' ;		// value attribute
+	
+	
+	if ( ! empty( $attributes ) ) {
+		// other attributes as passed in.
+		$echoout = $echoout . ' ' . $attributes; 
+	}
+	
+	if ( 'img' === $html_element_type ) {
+		// img elements need the src attribute
+		$echoout = $echoout . ' ' . 'src = ' . '"' . $image_url . '"' ;
+	}
+	
+	if ( 'a' === $html_element_type && ! empty( $target_url ) ) {
+		// add the href element
+		$echoout = $echoout . ' ' . 'href = ' . '"' . $target_url . '"' ;
+	}
+
+	$echoout = $echoout. '>'; // closing bracket for the tag
+	
+	if ( 'a' === $html_element_type ) {
+		// add the img element if we're using an "a" tag
+		$echoout = $echoout . ' ' . '<img src = ' . $image_url . '>';
+	}
+	
+	$echoout = $echoout. ' ' . '</' . $html_element_type . '>'; // closing tag such as </a> or </img>
+	
+	$echoout = $echoout. ' ' . '</span>' ; // closing tag of encompassing span
+	
+	if ( ! $return_markup ) {
+		echo $echoout ;
+	} else {
+		return $echoout;
+	}
 }
 
 
--- includes/admin/metaboxes/toolbar-middle.php
+++ includes/admin/metaboxes/toolbar-middle.php
@@ -15,10 +15,10 @@ do_action( 'wpas_ticket_detail_toolbar01_before', $post );
 ?>
 
 <!-- Button to collapse replies -->
-<?php wpas_add_ticket_detail_toolbar_item( 'img', 'wpas-collapse-replies-top', __( 'Toggle Replies (Hide All Replies Except The Last 3)', 'awesome-support' ), WPAS_URL . "assets/admin/images/icons/toggle-replies.png" ); ?>
+<?php echo wpas_add_ticket_detail_toolbar_item( 'img', 'wpas-collapse-replies-top', __( 'Toggle Replies (Hide All Replies Except The Last 3)', 'awesome-support' ), WPAS_URL . "assets/admin/images/icons/toggle-replies.png" ); ?>
 
 <!-- Button to toggle ticket slug -->
-<?php wpas_add_ticket_detail_toolbar_item( 'img', 'wpas-toggle-ticket-slug', __( 'Show/Hide The Ticket Slug)', 'awesome-support' ), WPAS_URL . "assets/admin/images/icons/toggle-ticket-slug.png" ); ?>
+<?php echo wpas_add_ticket_detail_toolbar_item( 'img', 'wpas-toggle-ticket-slug', __( 'Show/Hide The Ticket Slug)', 'awesome-support' ), WPAS_URL . "assets/admin/images/icons/toggle-ticket-slug.png" ); ?>
 
 
 <?php 
@@ -30,70 +30,3 @@ do_action( 'wpas_ticket_detail_toolbar01_before', $post );
 do_action( 'wpas_ticket_detail_toolbar01_after', $post ); 
 ?>
 
-<?php
-/**
- * Adds an item to the tool-bar in the ticket detail in wp-admin
- * 
- * Note that the menu item is echoed directly to the screen so this
- * function should be called using a do_action hook if called
- * from any file other than this one!
- *
- * @since 4.4.0
- *
- * @param 	string $html_element_type   img or a (anchor)
- *          string $item_css_id 		The CSS ID of the toolbar item 
- *                                      Any "-" in the name will be converted to underscores to create a class name; the "-" will remain for the CSS ID;
- *                                      For example: my-css-id will result in a markup with the css id = 'my-css-id' and the class name = 'my_css_id'.
- *		  	string $tool_tip_text 		The tool tip that will be displayed for the new toolbar item
- *			string $image_url			The URL for the toolbar item image.
- *			string $target_url			(optional) The URL target if creating an 'a' (anchor) element
- *			string $attributes			(optional) Any other attribute that needs to be added to the markup
- * 			bool   $return_markup		If set to true, it will return the button markup instead of echoing it to the screen.
- *
- * @return 	void (basically nothing is returned)
- */
-function wpas_add_ticket_detail_toolbar_item( $html_element_type, $item_css_id, $tool_tip_text, $image_url, $target_url='', $attributes = '', $return_markup = false ) {
-	
-	$name = str_replace( '-', '_', $item_css_id );  // convert the passed ids into text to be used for classnames.  For convention we're using classnames with underscores (_) and ids with dashes (-)
-
-	$echoout = '';
-	$echoout = $echoout . '<span data-hint=' . '"' . $tool_tip_text . '"' . 'class="wpas-replies-middle-toolbar-item hint-bottom hint-anim">';
-	$echoout = $echoout . ' ' . '<' . $html_element_type . ' ' ;  		// opening tag such as <a> or <img>
-	$echoout = $echoout . ' ' . 'name = ' . '"' . $name . '"' ; 		// name attribute
-	$echoout = $echoout . ' ' . 'id = ' . '"' . $item_css_id . '"' ;	// css ID
-	$echoout = $echoout . ' ' . 'class="link-primary wpas-link-reply wpas-middle-toolbar-links ' . $name . '"' ; // css class names
-	$echoout = $echoout . ' ' . 'value = ' . '"' . $name . '"' ;		// value attribute
-	
-	
-	if ( ! empty( $attributes ) ) {
-		// other attributes as passed in.
-		$echoout = $echoout . ' ' . $attributes; 
-	}
-	
-	if ( 'img' === $html_element_type ) {
-		// img elements need the src attribute
-		$echoout = $echoout . ' ' . 'src = ' . '"' . $image_url . '"' ;
-	}
-	
-	if ( 'a' === $html_element_type && ! empty( $target_url ) ) {
-		// add the href element
-		$echoout = $echoout . ' ' . 'href = ' . '"' . $target_url . '"' ;
-	}
-
-	$echoout = $echoout. '>'; // closing bracket for the tag
-	
-	if ( 'a' === $html_element_type ) {
-		// add the img element if we're using an "a" tag
-		$echoout = $echoout . ' ' . '<img src = ' . $image_url . '>';
-	}
-	
-	$echoout = $echoout. ' ' . '</' . $html_element_type . '>'; // closing tag such as </a> or </img>
-	
-	$echoout = $echoout. ' ' . '</span>' ; // closing tag of encompassing span
-	
-	if ( ! $return_markup ) {
-		echo $echoout ;
-	} else {
-		return $echoout;
-	}
-}
