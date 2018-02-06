344a2e4d698b9b826096161287d71837b12939c2
Nigel Bahadur <elindydotcom@hotmail.com>
2018-02-05 21:14:17 -0600

    Add some basic options for disclosures related to GDPR when registering a new user.

---
+++ includes/admin/settings/settings-registration.php
@@ -0,0 +1,143 @@
+<?php
+add_filter( 'wpas_plugin_settings', 'wpas_core_settings_registration', 5, 1 );
+/**
+ * Add plugin core settings for registration.
+ *
+ * @param  array $def Array of existing settings
+ *
+ * @return array      Updated settings
+ */
+function wpas_core_settings_registration( $def ) {
+
+	$user_registration = boolval( get_option( 'users_can_register' ) );
+	$registration_lbl  = ( true === $user_registration ) ? _x( 'allowed', 'User registration is allowed', 'awesome-support' ) : _x( 'not allowed', 'User registration is not allowed', 'awesome-support' );
+
+	$settings = array(
+		'registration' => array(
+			'name'    => __( 'Registration', 'awesome-support' ),
+			'options' => array(
+				array(
+					'name' => __( 'Registration', 'awesome-support' ),
+					'type' => 'heading',
+				),
+
+				array(
+					'name'    => __( 'Allow Registrations', 'awesome-support' ),
+					'id'      => 'allow_registrations',
+					'type'    => 'radio',
+					'desc'    => sprintf( __( 'Allow users to register on the support page. This setting can be enabled even though the WordPress setting is disabled. Currently, registrations are %s by WordPress.', 'awesome-support' ),  "<strong>$registration_lbl</strong>" ),
+					'default' => 'allow',
+					'options' => array(
+						'allow'           => __( 'Allow registrations', 'awesome-support' ),
+						'disallow'        => __( 'Disallow registrations', 'awesome-support' ),
+						'disallow_silent' => __( 'Disallow registrations without notice (just show the login form)', 'awesome-support' ),
+					)
+				),
+				
+				array(
+					'name' => __( 'Registration Field Descriptions', 'awesome-support' ),
+					'type' => 'heading',
+					'desc' => __( 'Add a description to each of the registration fields which will appear beneath the field. You can use this to add GDPR related information indicating what each field is used for.', 'awesome-support' ),
+				),
+				array(
+					'name'     => __( 'First name description', 'awesome-support' ),
+					'id'       => 'reg_first_name_desc',
+					'type'     => 'text',
+					'default'  => '',
+				),
+				array(
+					'name'     => __( 'Last name description', 'awesome-support' ),
+					'id'       => 'reg_last_name_desc',
+					'type'     => 'text',
+					'default'  => '',
+				),
+				array(
+					'name'     => __( 'Email address description', 'awesome-support' ),
+					'id'       => 'reg_email_desc',
+					'type'     => 'text',
+					'default'  => '',
+				),				
+
+				array(
+					'name' => __( 'GDPR Notice #1', 'awesome-support' ),
+					'type' => 'heading',
+					'desc' => __( 'The GDPR requires that you provide explicit notice about what you will do with the data you collect from users. This section allows you to describe how any personal data collected will be used and require consent from the user before they can register.', 'awesome-support' ),
+				),
+				array(
+					'name'     => __( 'GDPR Notice: Short Description', 'awesome-support' ),
+					'id'       => 'gdpr_notice_short_desc_01',
+					'type'     => 'text',
+					'default'  => '',
+					'desc'     => __( 'If you fill this in, a mandatory checkbox will be added in the registration form. Users won\'t be able to register if they don\'t tick the checkbox.  It is best to keep this brief - eg: Receive Emails? or Join Email List?', 'awesome-support' ),
+				),				
+				array(
+					'name'     => __( 'GDPR Notice: Long Description', 'awesome-support' ),
+					'id'       => 'gdpr_notice_long_desc_01',
+					'type'     => 'editor',
+					'default'  => '',
+					'desc'     => __( 'If you add notice terms in this box, a mandatory checkbox will be added in the registration form. Users won\'t be able to register if they don\'t accept these notice terms.  It is best to keep this notice to one or two lines.', 'awesome-support' ),
+					'settings' => array( 'quicktags' => true, 'textarea_rows' => 7 )
+				),
+				
+				array(
+					'name' => __( 'GDPR Notice #2', 'awesome-support' ),
+					'type' => 'heading',
+					'desc' => __( 'If personal data will be used in any additional context add that here. It is best to keep this brief.', 'awesome-support' ),
+				),
+				array(
+					'name'     => __( 'GDPR Notice: Short Description', 'awesome-support' ),
+					'id'       => 'gdpr_notice_short_desc_02',
+					'type'     => 'text',
+					'default'  => '',
+					'desc'     => __( 'If you fill this in, a mandatory checkbox will be added in the registration form. Users won\'t be able to register if they don\'t tick the checkbox.  It is best to keep this brief - eg: Receive Emails? or Join Email List?', 'awesome-support' ),
+				),				
+				array(
+					'name'     => __( 'GDPR Notice: Long Description', 'awesome-support' ),
+					'id'       => 'gdpr_notice_long_desc_02',
+					'type'     => 'editor',
+					'default'  => '',
+					'desc'     => __( 'If you add notice terms in this box, a mandatory checkbox will be added in the registration form. Users won\'t be able to register if they don\'t accept these notice terms.  It is best to keep this notice to one or two lines.', 'awesome-support' ),
+					'settings' => array( 'quicktags' => true, 'textarea_rows' => 7 )
+				),
+				
+				array(
+					'name' => __( 'GDPR Notice #3', 'awesome-support' ),
+					'type' => 'heading',
+					'desc' => __( 'If personal data will be used in any additional context add that here. It is best to keep this brief.', 'awesome-support' ),
+				),
+				array(
+					'name'     => __( 'GDPR Notice: Short Description', 'awesome-support' ),
+					'id'       => 'gdpr_notice_short_desc_03',
+					'type'     => 'text',
+					'default'  => '',
+					'desc'     => __( 'If you fill this in, a mandatory checkbox will be added in the registration form. Users won\'t be able to register if they don\'t tick the checkbox.  It is best to keep this brief - eg: Receive Emails? or Join Email List?', 'awesome-support' ),
+				),				
+				array(
+					'name'     => __( 'GDPR Notice: Long Description', 'awesome-support' ),
+					'id'       => 'gdpr_notice_long_desc_03',
+					'type'     => 'editor',
+					'default'  => '',
+					'desc'     => __( 'If you add notice terms in this box, a mandatory checkbox will be added in the registration form. Users won\'t be able to register if they don\'t accept these notice terms.  It is best to keep this notice to one or two lines.', 'awesome-support' ),
+					'settings' => array( 'quicktags' => true, 'textarea_rows' => 7 )
+				),				
+				
+				array(
+					'name' => __( 'Terms & Conditions', 'awesome-support' ),
+					'type' => 'heading',
+				),
+				array(
+					'name'     => __( 'Content', 'awesome-support' ),
+					'id'       => 'terms_conditions',
+					'type'     => 'editor',
+					'default'  => '',
+					'desc'     => __( 'Terms & conditions are not mandatory. If you add terms, a mandatory checkbox will be added in the registration form. Users won\'t be able to register if they don\'t accept your terms', 'awesome-support' ),
+					'settings' => array( 'quicktags' => true, 'textarea_rows' => 7 )
+				),
+
+			)
+		),
+	);
+
+	return array_merge( $def, $settings );
+
+}
