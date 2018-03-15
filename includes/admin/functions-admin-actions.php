293eef0194c7231b9e10ecc3bb73cbd11b37e18a
Nigel Bahadur <elindydotcom@hotmail.com>
2018-03-15 01:43:05 -0500

    Fix: Continue fix for issue where agents logged into back end can view any ticket if they know the ticket ID or URL - even if the the setting to view all tickets is turned off.

--- includes/admin/functions-admin-actions.php
+++ includes/admin/functions-admin-actions.php
@@ -25,7 +25,7 @@ add_action( 'admin_head-post.php', 'admin_head_post_editing' );
  */
 function admin_head_post_editing() {
 
-	global $post_type, $post_ID;
+	global $post_type, $post_ID, $current_user;
 
 	if( 'ticket' === $post_type ) {
 		$can = wpas_can_view_ticket( $post_ID );
@@ -34,13 +34,13 @@ function admin_head_post_editing() {
 			//can view ticket - do nothing and continue.
 		}
 		else {
-			//Not allowed to view ticket - bail out.
+			//Not allowed to view ticket - write to log file and bail out.
+			wpas_write_log('security', 'A logged in user attempted to access a ticket without the necessary permissions. ' . 'Ticket id: ' . (string) $post_ID . ', Logged In user ID: ' . (string) $current_user->ID ) ;
 			wp_redirect( add_query_arg( array( 'post_type' => 'ticket' ), admin_url( 'edit.php' ) ) );
 		}
 	}
 
 }
-
 
 add_action( 'wpas_do_admin_close_ticket', 'wpas_admin_action_close_ticket' );
 /**
