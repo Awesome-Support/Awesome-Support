60118f2f33ef6b222ba7d905d1684b128f6de364
Nigel Bahadur <elindydotcom@hotmail.com>
2018-03-24 21:39:45 -0500

    Simple WooCommerce Integration Using Custom Endpoints

--- includes/admin/settings/settings-integration.php
+++ includes/admin/settings/settings-integration.php
@@ -12,6 +12,31 @@ function wpas_core_settings_integration( $def ) {
 		'integration' => array(
 			'name'    => __( 'Integrations', 'awesome-support' ),
 			'options' => array(
+			
+				array(
+					'name'    => __( 'WooCommerce', 'awesome-support' ),
+					'id'      => 'simple_wc',
+					'type'    => 'heading',
+					'desc'    => sprintf( __( 'Add the MY TICKETS and OPEN A TICKET links to the MY ACCOUNT page in WooCommerce. For more advanced integration features checkout our %s', 'awesome-support' ), '<a href="https://getawesomesupport.com/addons/woocommerce/" target="_blank">' . 'WooCommerce Addon' . '</a>' ),
+				),
+				array(
+					'name'    => __( 'Enable WooCommerce Integration', 'awesome-support' ),
+					'id'      => 'enable_simple_wc',
+					'type'    => 'checkbox',
+					'desc'    => __( 'Add the Awesome Support MY TICKETS and OPEN A TICKET page links on the WooCommerce MY ACCOUNT page', 'awesome-support' ),					
+					'default' => false,
+				),
+				array(
+					'name'    => __( 'Permalinks Warning:', 'awesome-support' ),
+					'type'	  => 'note',
+					'desc'    => __( 'Note: You must go to your Permalinks page and save the settings there again if you turn on or off the WooCommerce option above.', 'awesome-support' ),
+				),				
+
+				array(
+					'name'    => __( 'Teamviewer', 'awesome-support' ),
+					'id'      => 'simple_tv',
+					'type'    => 'heading',
+				),				
 				array(
 					'name'    => __( 'Enable Teamviewer Chat', 'awesome-support' ),
 					'id'      => 'enable_teamviewer_chat',
