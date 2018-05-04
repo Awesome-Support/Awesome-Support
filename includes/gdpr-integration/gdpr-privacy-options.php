ddf819f4994127d86bf3fde233f944de885f1daf
Naveen Giri <1naveengiri@gmail.com>
2018-05-04 15:49:09 +0530

    Add gdpr privacy option button in frontend

---
+++ includes/gdpr-integration/gdpr-privacy-options.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Awesome Support Privacy Option.
+ *
+ * @package   Awesome_Support
+ * @author    Naveen Giri <1naveengiri>
+ * @license   GPL-2.0+
+ * @link      https://getawesomesupport.com
+ */
+class WPAS_Privacy_Option {
+	/**
+	 * Instance of this class.
+	 *
+	 * @since    1.0.0
+	 * @var      object
+	 */
+	protected static $instance = null;
+	/**
+	 * Store the potential error messages.
+	 */
+	protected $error_message;
+
+	public function __construct() {
+		add_filter( 'wpas_frontend_add_nav_buttons', array( $this, 'frontend_privacy_add_nav_buttons' ) );
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    object    A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Add GDPR privacy options to
+	 * * Add/Remove Existing Consent
+	 * * Export tickets and user data
+	 * * Delete my existing data
+	 *
+	 * @return void
+	 */
+	public function frontend_privacy_add_nav_buttons() {
+		$button_title = wpas_get_option( 'privacy_button_label', 'Privacy' );
+		wpas_make_button( __( $button_title, 'awesome-support' ), array( 'type' => 'link', 'link' => '#', 'class' => 'wpas-btn wpas-btn-default wpas-link-privacy' ) );
+	}
+}
