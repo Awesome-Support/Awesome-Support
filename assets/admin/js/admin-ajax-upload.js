c284e4d0f67304b57075ef0f3e261ca7a2202f4e
alaca <ante.laca@gmail.com>
2018-06-21 16:40:04 +0200

    Enable dropzone on dynamically loaded elements

--- assets/admin/js/admin-ajax-upload.js
+++ assets/admin/js/admin-ajax-upload.js
@@ -4,76 +4,105 @@
     var ticket_id = false;
     Dropzone.autoDiscover = false;
 
-    // Attach dropzone to every upload field
-    $('.wpas-uploader-dropzone').each(function(i, e){
 
-        var id    = $(e).attr('id');
-        var paste = $(this).data('enable-paste');
-        ticket_id = (  $('#post_ID').length ) ? $('#post_ID').val() : $(this).data('ticket-id');
+    function attachDropZones() {
 
+        // Attach dropzone to every upload field
+        $('.wpas-uploader-dropzone').each(function(i, e){
 
-        $('#' + id).dropzone({ 
-            url: WPAS_AJAX.ajax_url,
-            paramName: 'wpas_files',
-            acceptedFiles : WPAS_AJAX.accept,
-            maxFiles : WPAS_AJAX.max_files,
-            maxFilesize : WPAS_AJAX.max_size,
-            addRemoveLinks: true,
-            init: function() {
-
-                var that = this;
-                
-                this.on('sending', function(file, xhr, formData){
-                    formData.append('action', 'wpas_upload_attachment');
-                    formData.append('ticket_id', ticket_id );
-                    uploaded = true;
-                });
-        
-                this.on('addedfile', function(e) {
-                    if (this.files.length > this.options.maxFiles) {
-                        this.removeFile(this.files[0]);
-                        alert( WPAS_AJAX.exceeded );
-                    }
-                });
-
-                // Check if paste is enabled
-                if( paste === 1 ) {
-
-                    document.onpaste = function(e){
-
-                        var items = (e.clipboardData || e.originalEvent.clipboardData).items;
-
-                        for (var j in items) {
-                            if (items[j].kind === 'file') {
-                                that.addFile( items[j].getAsFile() ); 
-                            }
-                        }
-
-                    };
-      
-                } 
-        
-            },
-            removedfile: function(file) {
-
-                var _ref;
-                var name = file.name; 
-
-                $.ajax({
-                    type: 'POST',
-                    url: WPAS_AJAX.ajax_url,
-                    data: {
-                        action: 'wpas_delete_temp_attachment',
-                        ticket_id: ticket_id,
-                        attachment: file.name
-                    }
-                });
-
-                return (_ref = file.previewElement) != null ? _ref.parentNode.removeChild(file.previewElement) : void 0;
+            // Check if its already attached
+            if ( $(e).hasClass('dz-clickable') ) {
+                return false;
             }
+
+            var id    = $(e).attr('id');
+            var paste = $(this).data('enable-paste');
+            ticket_id = (  $('#post_ID').length ) ? $('#post_ID').val() : $(this).data('ticket-id');
+
+            $('#' + id).dropzone({ 
+                url: WPAS_AJAX.ajax_url,
+                paramName: 'wpas_files',
+                acceptedFiles : WPAS_AJAX.accept,
+                maxFiles : WPAS_AJAX.max_files,
+                maxFilesize : WPAS_AJAX.max_size,
+                addRemoveLinks: true,
+                init: function() {
+
+                    var that = this;
+                    
+                    this.on('sending', function(file, xhr, formData){
+                        formData.append('action', 'wpas_upload_attachment');
+                        formData.append('ticket_id', ticket_id );
+                        uploaded = true;
+                    });
+            
+                    this.on('addedfile', function(e) {
+                        if (this.files.length > this.options.maxFiles) {
+                            this.removeFile(this.files[0]);
+                            alert( WPAS_AJAX.exceeded );
+                        }
+                    });
+
+                    // Check if paste is enabled
+                    if( paste === 1 ) {
+
+                        $(document).on('paste', function (e){
+
+                            if ( $('#' + id).hasClass('wpas-mouse-hover') ) {
+
+                                var items = (e.clipboardData || e.originalEvent.clipboardData).items;
+
+                                for (var j in items) {
+                                    if (items[j].kind === 'file') {
+                                        that.addFile( items[j].getAsFile() ); 
+                                    }
+                                }
+                            }
+
+                        });
+                        
+                    } 
+            
+                },
+                removedfile: function(file) {
+
+                    var _ref;
+                    var name = file.name; 
+
+                    $.ajax({
+                        type: 'POST',
+                        url: WPAS_AJAX.ajax_url,
+                        data: {
+                            action: 'wpas_delete_temp_attachment',
+                            ticket_id: ticket_id,
+                            attachment: file.name
+                        }
+                    });
+
+                    return (_ref = file.previewElement) != null ? _ref.parentNode.removeChild(file.previewElement) : void 0;
+                }
+
+            });
+
+            $(document).on({
+                mouseenter: function () {
+                    $(this).addClass('wpas-mouse-hover');
+                },
+                mouseleave: function () {
+                    $(this).removeClass('wpas-mouse-hover');
+                }
+            }, '#' + id );
 
         });
 
+    }
+
+    // Attach dropzones
+    attachDropZones();
+
+    // Attach dropzones on dynamic elements
+    $(document).ajaxComplete(function(){
+        attachDropZones();
     });
 
 
