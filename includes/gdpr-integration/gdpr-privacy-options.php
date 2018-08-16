3ec269543d1e7f73e8cbdd9f4d5b11ebd74bde1f
Rolly G. Bueno Jr <rollybueno@gmail.com>
2018-08-16 15:42:17 +0800

    Bugfix: Privacy Popup showing at the bottom of certain search pages

--- includes/gdpr-integration/gdpr-privacy-options.php
+++ includes/gdpr-integration/gdpr-privacy-options.php
@@ -848,7 +848,7 @@ class WPAS_Privacy_Option {
 	 * @return void
 	 */
 	public static function print_privacy_popup_temp() {
-		if ( wpas_is_plugin_page() && ! is_search() ){ ?>
+		if ( wpas_is_front_end_plugin_page() ){ ?>
 			<div class="privacy-container-template">
 				<div class="entry entry-normal" id="privacy-option-content">
 					<div class="wpas-gdpr-loader-background"></div><!-- .wpas-gdpr-loader-background -->
