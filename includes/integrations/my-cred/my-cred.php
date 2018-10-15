f27a78797234c21ca2642ef1285659ccbbb4874b
Nigel Bahadur <elindydotcom@hotmail.com>
2018-10-15 04:44:38 -0500

    myCRED points for adding replies to a ticket.

--- includes/integrations/my-cred/my-cred.php
+++ includes/integrations/my-cred/my-cred.php
@@ -17,7 +17,8 @@
 	public function __construct() {	
 	
 		// Add some action hooks here!
-		add_action('wpas_after_close_ticket', array($this, 'after_close_ticket'), 20, 3 );
+		add_action('wpas_after_close_ticket',	array($this, 'after_close_ticket'), 20, 3 );
+		add_action('wpas_add_reply_after',		array($this, 'after_reply_ticket'), 20, 2 );
 		
 	}
 	
@@ -48,8 +49,45 @@
 			
 			// Add points for agent even if user closes a ticket...
 			if ( false == wpas_is_agent( $user_id ) && !empty( wpas_get_option('myCRED_agent_point_type' ) ) && true == wpas_get_option('myCRED_agent_gets_points_user_close' )  ) {
-				mycred_add( $ticket_id, $user_id, wpas_get_option('myCRED_agent_points_ticket_close'), 'Agent gets points when user closed ticket #' . (string) $ticket_id, $ticket_id, '', wpas_get_option('myCRED_agent_point_type') );				
-			}				
+				
+				// Who is the primary agent on the ticket?
+				$agent_id = wpas_get_primary_agent_by_ticket_id( $ticket_id );
+				
+				if ( $agent_id ) {
+					mycred_add( $ticket_id, $agent_id, wpas_get_option('myCRED_agent_points_ticket_close'), 'Agent gets points when user closed ticket #' . (string) $ticket_id, $ticket_id, '', wpas_get_option('myCRED_agent_point_type') );				
+				}
+				
+			}
+			
+		}
+	}
+		
+	/**
+	 * Add points when a reply is added to a ticket
+	 *
+	 * Action hook: wpas_add_reply_after
+	 *
+	 * @param $reply_id integer
+	 * @param $data array
+	 *
+	 * @return void
+	 */
+	public function after_reply_ticket($reply_id, $data) {
+
+		if ( function_exists('mycred_add') ) {
+			
+			$ticket_id = wpas_get_ticket_id( $reply_id ) ;
+			$user_id = wp_get_current_user()->ID;
+			
+			// Add points for agent sending a reply ticket.
+			if ( true == wpas_is_agent( $user_id ) && !empty( wpas_get_option('myCRED_agent_point_type' ) ) ) {
+				mycred_add( $ticket_id, $user_id, wpas_get_option('myCRED_agent_points_ticket_reply'), 'Points for agent replying to ticket #' . (string) $ticket_id, $ticket_id, '', wpas_get_option('myCRED_agent_point_type') );
+			}
+			
+			// Add points for user replying to a ticket
+			if ( false == wpas_is_agent( $user_id ) && !empty( wpas_get_option('myCRED_user_point_type' ) ) ) {
+				mycred_add( $ticket_id, $user_id, wpas_get_option('myCRED_user_points_ticket_reply'), 'Points for user replying to ticket #' . (string) $ticket_id, $ticket_id, '', wpas_get_option('myCRED_user_point_type') );
+			}
 			
 		}
 		
