985fab768cf87c0f193e4660c349856d1370a8c7
Mohammad Umer <mumer@cheapempires.com>
2020-05-27 23:29:01 +0500

    # Added upgrade function for Awesome Support 6.0.0

--- includes/admin/upgrade/functions-upgrade.php
+++ includes/admin/upgrade/functions-upgrade.php
@@ -554,3 +554,20 @@ function wpas_upgrade_590() {
 	// we create this 590 routine to make sure it runs for early 520 adopters.
 	wpas_upgrade_581();
 }
+
+/**
+ * Upgrade function for version 6.0.0
+ *
+ * No new capabilities need to be added to certain roles.
+ *
+ * @since 6.0.0
+ * @return void
+ */
+function wpas_upgrade_600() {
+	// Run the 581 upgrade option for version 600.
+	// The 581 upgrade was the internal upgrade option during testing of the 600 release.
+	// Therefore the two routines are the same and there is no reason to write a separate 600 routine.
+	// But we do want early 581 adopters to get the later changes to the update routine.  So
+	// we create this 600 routine to make sure it runs for early 520 adopters.
+	wpas_upgrade_581();
+}
