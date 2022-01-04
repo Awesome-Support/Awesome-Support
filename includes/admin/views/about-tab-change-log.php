8dfa4f579fb10bd98b3c74ca65ba7907140d7f16
Nguyen Huu Minh <ted@gostats.com>
2022-01-04 11:25:56 +0700

    Update to 6.0.11

--- includes/admin/views/about-tab-change-log.php
+++ includes/admin/views/about-tab-change-log.php
@@ -5,6 +5,20 @@
 	<div class="row">
 		<div>
 			<div class="about-body">
+				<h1>What's New In 6.0.11</h1>
+				<h3>6.0.11 includes maintenance fixes and security patches. Here is a more comprehensive list:</h3>
+				<ul style="padding-left: 3em; list-style-type: disc;">
+					<li>Fix: Fixed security vulnerability</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<hr />
+
+	<div class="row">
+		<div>
+			<div class="about-body">
 				<h1>What's New In 6.0.10</h1>
 				<h3>6.0.10 includes maintenance fixes and security patches. Here is a more comprehensive list:</h3>
 				<ul style="padding-left: 3em; list-style-type: disc;">
