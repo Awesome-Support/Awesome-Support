ce0cc5674eea2287538c7555cd1e40b7c56369ab
Tednh <minhnh6fpt@gmail.com>
2025-12-14 00:49:56 +0700

    Ted update version 6.3.7 and changelog

--- includes/admin/views/about-tab-change-log.php
+++ includes/admin/views/about-tab-change-log.php
@@ -9,6 +9,8 @@
 				<h3>6.3.7 Revised plugin deactivation process with feedback.</h3>
 				<ul>
 					<li>Update default fallback rules for the .htaccess file in the ticket folder.</li>
+					<li>Fix Missing Authorization to Unauthenticated Role Demotion.</li>
+					<li>Fix vulnerable issue of Unauthenticated Sensitive Information Disclosure.</li>
 				</ul>
 			</div>
 		</div>
