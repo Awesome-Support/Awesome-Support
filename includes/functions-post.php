1e73210f010ea82d6d6fb298c2f6b5c801d48ea8
Tednh <minhnh6fpt@gmail.com>
2026-02-08 00:30:08 +0700

    Ted fix 641-when-a-ticket-is-public-non-user-can-not-load-more-reply-because-our-checking-registered-user-to-load-reply-condition

--- includes/functions-post.php
+++ includes/functions-post.php
@@ -1822,14 +1822,22 @@ function wpas_get_ticket_replies_ajax() {
 	
 	$ticket_id = absint( $_POST['ticket_id'] );	
 
-	//Check permission for capability of current user
-	if ( ! current_user_can( 'reply_ticket' ) ) {
-		wp_send_json_error( array('message' => __('Unauthorized action. You do not have permission to load TinyMCE via Ajax request to edit a reply.', 'awesome-support') ), 403);		
-    }	
-		
-	if( !check_ajax_referer( 'wpas_loads_replies', 'ticket_replies_nonce', false ) ) {		
-		wp_send_json_error( array( 'message' => "You don't have access to perform this action" ) );
-		die();
+	/* Check if the ticket is public (wpas_pbtk_flag) - only allow public access if the Public Tickets add-on is active */
+	$pbtk_flag = get_post_meta( $ticket_id , '_wpas_pbtk_flag', true );		
+	if ( class_exists( 'AS_Publictickets_Loader' ) && 'public' === $pbtk_flag ) {
+		$can_view = true;
+	}
+	else
+	{
+		//Check permission for capability of current user
+		if ( ! current_user_can( 'reply_ticket' ) ) {
+			wp_send_json_error( array('message' => __('Unauthorized action. You do not have permission to load TinyMCE via Ajax request to edit a reply.', 'awesome-support') ), 403);		
+	    }	
+			
+		if( !check_ajax_referer( 'wpas_loads_replies', 'ticket_replies_nonce', false ) ) {		
+			wp_send_json_error( array( 'message' => "You don't have access to perform this action" ) );
+			die();
+		}
 	}
 	
 	$offset    = isset( $_POST['ticket_replies_total'] ) ? (int) $_POST['ticket_replies_total'] : 0;
