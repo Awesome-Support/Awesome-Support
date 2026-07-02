bffdc332f7521f87ac12dd3b8b60b42e868dab48
Tednh <minhnh6fpt@gmail.com>
2026-07-02 17:49:41 +0700

    ted sync with local

--- includes/install.php
+++ includes/install.php
@@ -333,6 +333,8 @@ function wpas_create_pages() {
  * @return void
  */
 function wpas_flush_rewrite_rules() {
+
+	plugin_log( __FILE__.' CALL function '.__FUNCTION__. json_encode( (array)$_GET ) );
 	flush_rewrite_rules();
 }
 
