6d9924c9ea4cf78d013e7087b5d364777a37908d
Nguyen Huu Minh <minhnh6fpt@gmail.com>
2026-07-28 15:47:45 +0700

    Ted fix 704-php-84-error and 75-meo-test-email-outlook-piping-on-php-83-wp-702

--- includes/admin/metaboxes/version.php
+++ includes/admin/metaboxes/version.php
@@ -14,5 +14,24 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-echo( esc_html__('Awesome Support Version: ', 'awesome-support') . esc_attr( WPAS_VERSION ) ) ;
+global $post;
 
+echo esc_html__( 'Awesome Support Version: ', 'awesome-support' ) . esc_attr( WPAS_VERSION );
+
+// Display Ticket Source only if ticket was created from email piping
+if ( isset( $post->ID ) ) {
+	$source = get_post_meta( $post->ID, '_wpas_source', true );
+
+	if ( 'email' === $source ) {
+		$mailbox_name = get_post_meta( $post->ID, '_wpas_mailboxname', true );
+
+		echo '<br/>';
+		echo '<strong>' . esc_html__( 'Ticket Source: ', 'awesome-support' ) . '</strong>';
+
+		if ( ! empty( $mailbox_name ) ) {
+			echo '<span style="color:#0073aa;">&#9993; ' . esc_html( $mailbox_name ) . '</span>';
+		} else {
+			echo '<span style="color:#0073aa;">&#9993; ' . esc_html__( 'default mailbox', 'awesome-support' ) . '</span>';
+		}
+	}
+}
